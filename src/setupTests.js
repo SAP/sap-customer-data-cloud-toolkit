df076352e400685609c21e9bafd6d15d11d95f58
Luis Marques <l.marques@sap.com>
2022-09-19 10:52:50 +0100

    CDCTOOLBOX-65: Fix testing of react components with ui5 elements by adding test libs and configs in package.json (#8)

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+// import Adapter from 'enzyme-adapter-react-17'; // only use for React 17 after release
+
+configure({ adapter: new Adapter() });
