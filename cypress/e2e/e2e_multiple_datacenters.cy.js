cccdc88bdb4cb4a586825a2e0245825167971912
Lopes <g.lopes@sap.com>
2022-10-07 10:10:56 +0100

    CDCTOOLBOX-5: Cypress E2E Testing

---
+++ cypress/e2e/e2e_multiple_datacenters.cy.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+describe('Site Deployer create multiple datacenters', () => {
+  it('Validates the url"', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.contains('Site Deployer').click()
+    cy.url().should('include', '/cdc-tools/site-deployer')
+  })
+
+  it('Writes the site name', () => {
+    cy.get('#cdctools-siteDomain')
+      .shadow()
+      .find('#ui5wc_11-inner')
+      .type('a_b_c_site_deployer_multiple_datacenters')
+      .should('have.value', 'a_b_c_site_deployer_multiple_datacenters')
+  })
+
+  it('Selects a Site Structure"', () => {
+    cy.get('#cdctools-siteStructure').then((dropdown) => {
+      cy.wrap(dropdown).click()
+
+      cy.get('.ui5wc_24').shadow().find('.ui5-select-popover').find('#ui5wc_20-li').click()
+    })
+    cy.get('#cdctools-siteStructure').shadow().find('.ui5-select-label-root').should('have.text', 'Structure 1')
+  })
+
+  it('Select the create site button"', () => {
+    cy.get('ui5-button[icon = "add"]').shadow().find('.ui5-button-root').find('#ui5wc_12-content').click()
+    cy.get('ui5-table-row').should('have.length', '3')
+  })
+
+  it('Saves the Site"', () => {
+    cy.get('#save-main').click()
+  })
+})
