bbddd9b3b8a671adad7101b05a0d322ea5bf702d
I546218 <diogo.carvalho@sap.com>
2022-11-08 14:40:54 +0000

    folder, file and archive creation with UT

--- src/services/fileManager/file.js
+++ src/services/fileManager/file.js
@@ -1,66 +1,76 @@
 import os from 'os'
 import fs from 'fs'
 import AdmZip from 'adm-zip'
+//import path from 'path'
 
 class File {
   static #OS_TEMP_DIR_PATH = os.tmpdir()
-  static #DIR = File.#OS_TEMP_DIR_PATH + '/' + 'cdc-tools-chrome-extension'
-
-  createDir(dir) {
-    console.log(dir)
+  static #DIR = `${File.#OS_TEMP_DIR_PATH}/cdc-tools-chrome-extension`
+  #createDir(dir) {
     if (!fs.existsSync(dir)) {
-      console.log('-------')
       fs.mkdirSync(dir)
     }
   }
 
-  createTemplateDir(template) {
-    const path = File.#DIR + '/' + template
-    this.createDir(path)
+  #createTemplateDir(template) {
+    const path = `${File.#DIR}/${template}`
+    this.#createDir(path)
     return path
   }
 
-  appendToFile(filePath, content) {
+  #appendToFile(filePath, content) {
     fs.writeFileSync(filePath, content, { flag: 'a+' }, (err) => {
       if (err) throw err
     })
   }
 
   createFile(template, lang, content) {
-    this.createDir(File.#DIR)
+    this.#createDir(File.#DIR)
 
-    const templatePath = this.createTemplateDir(template)
+    const templatePath = this.#createTemplateDir(template)
 
-    const filePath = templatePath + '/' + lang + '.html'
+    const filePath = `${templatePath}/${lang}.html`
+    this.#appendToFile(filePath, content)
+  }
 
-    this.appendToFile(filePath, content)
+  #deleteDir() {
+    fs.rmdirSync(File.#DIR, { recursive: true, force: true }, (err) => {
+      if (err) {
+        return console.log('error occurred while deleting directory', err)
+      }
+      console.log(`${File.#DIR} directory deleted successfully`)
+    })
   }
 
   async createZipArchive() {
     try {
       const zip = new AdmZip()
-      const outputFile = 'emailsTemplates.zip'
+      const fileName = 'emails_templates'
+      const outputFile = `${File.#OS_TEMP_DIR_PATH}/${fileName}.zip`
       zip.addLocalFolder(File.#DIR)
       zip.writeZip(outputFile)
       console.log(`Created ${outputFile} successfully`)
+      this.#deleteDir()
+
+      return outputFile
     } catch (e) {
       console.log(`Something went wrong. ${e}`)
     }
   }
 
-  async extractArchive(filepath) {
-    try {
-      const zip = new AdmZip(filepath)
-      const outputDir = `${path.parse(filepath).name}_extracted`
-      zip.extractAllTo(outputDir)
+  // [2 FUNCTIONS BELOW USEFUL FOR IMPORT EMAILS FEATURE AND TESTING]
 
-      console.log(`Extracted to "${outputDir}" successfully`)
-    } catch (e) {
-      console.log(`Something went wrong. ${e}`)
-    }
-  }
+  // async extractArchive(filepath) {
+  //   try {
+  //     const zip = new AdmZip(filepath)
+  //     const outputDir = `${path.parse(filepath).name}_extracted`
+  //     zip.extractAllTo(outputDir)
 
-  // [USEFUL FOR TESTING]
+  //     console.log(`Extracted to "${outputDir}" successfully`)
+  //   } catch (e) {
+  //     console.log(`Something went wrong. ${e}`)
+  //   }
+  // }
 
   // async readFile() {
   //     const fileContent = await new Promise((resolve, reject) => {
--- src/services/fileManager/file.test.js
+++ src/services/fileManager/file.test.js
@@ -1,11 +1,88 @@
 import File from '../fileManager/file'
+import os from 'os'
 import * as EmailsTestData from '../emails/data_test'
+import fs from 'fs'
 jest.mock('axios')
 jest.setTimeout(10000)
 
 describe('files test suite', () => {
-  test('create temo dir', async () => {
-    let file = new File()
-    file.createFile('passwordReset', 'es', EmailsTestData.emailTemplate)
+  const OS_TEMP_DIR_PATH = os.tmpdir()
+  const DIR = `${OS_TEMP_DIR_PATH}/cdc-tools-chrome-extension`
+  test('create file in temp dir', async () => {
+    const file = new File()
+    file.createFile('emailVerification', 'es', EmailsTestData.emailTemplate)
+
+    expect(fs.existsSync(DIR)).toBe(true)
+  })
+
+  test('check if 1 folder has 1 html file', async () => {
+    const file = new File()
+    file.createFile('emailVerification', 'es', EmailsTestData.emailTemplate)
+
+    expect(await checkIfFileExists('es', DIR, 'emailVerification')).toBe(true)
+  })
+
+  test('check if 1 folder has more than 1 html file', async () => {
+    const langs = ['en', 'pt', 'es', 'fr']
+    const templates = ['emailVerification']
+
+    createTemplates(templates, langs)
+
+    expect(await checkIfFileExists(langs[0], DIR, templates[0])).toBe(true)
+    expect(await checkIfFileExists(langs[1], DIR, templates[0])).toBe(true)
+    expect(await checkIfFileExists(langs[2], DIR, templates[0])).toBe(true)
+    expect(await checkIfFileExists(langs[3], DIR, templates[0])).toBe(true)
+  })
+
+  test('check if more than 1 folder has N html files', async () => {
+    const langs = ['en', 'pt-br', 'es', 'fr']
+    const templates = ['emailVerification', 'passwordReset']
+
+    createTemplates(templates, langs)
+
+    expect(await checkIfFileExists(langs[0], DIR, templates[0])).toBe(true)
+    expect(await checkIfFileExists(langs[1], DIR, templates[0])).toBe(true)
+    expect(await checkIfFileExists(langs[2], DIR, templates[0])).toBe(true)
+    expect(await checkIfFileExists(langs[3], DIR, templates[0])).toBe(true)
+    expect(await checkIfFileExists(langs[0], DIR, templates[1])).toBe(true)
+    expect(await checkIfFileExists(langs[1], DIR, templates[1])).toBe(true)
+    expect(await checkIfFileExists(langs[2], DIR, templates[1])).toBe(true)
+    expect(await checkIfFileExists(langs[3], DIR, templates[1])).toBe(true)
+  })
+
+  test('create .zip archive', async () => {
+    const file = new File()
+    file.createFile('emailVerification', 'es', EmailsTestData.emailTemplate)
+    const archiveFileName = await file.createZipArchive()
+
+    expect(fs.existsSync(archiveFileName)).toBe(true)
+  })
+
+  test('check if folder is deleted after archive creation', async () => {
+    const file = new File()
+    file.createFile('emailVerification', 'es', EmailsTestData.emailTemplate)
+    const archiveFileName = await file.createZipArchive()
+
+    expect(fs.existsSync(archiveFileName)).toBe(true)
+    expect(fs.existsSync(DIR)).toBe(false)
   })
 })
+
+function createTemplates(templates, langs) {
+  const file = new File()
+  for (const t of templates) {
+    for (const l of langs) {
+      file.createFile(t, l, EmailsTestData.emailTemplate)
+    }
+  }
+}
+
+async function checkIfFileExists(lang, path, template) {
+  const dir = await fs.promises.opendir(path + '/' + template)
+  for await (const dirent of dir) {
+    console.log(dirent.name)
+    if (dirent.name.includes(lang)) {
+      return true
+    }
+  }
+}
