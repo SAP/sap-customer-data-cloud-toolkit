41a9fe2195b5e8baa3df12466c3ee459332be31d
Daniel Cabral <daniel.cabral@sap.com>
2022-12-06 14:42:27 +0000

    CDCTOOLBOX-108:Emails import pop up css styles added and bugs fixed

--- src/components/emails-import-popup/emails-import-popup.component.jsx
+++ src/components/emails-import-popup/emails-import-popup.component.jsx
@@ -1,11 +1,13 @@
 import { withNamespaces } from 'react-i18next'
-import { Dialog, FileUploader, Bar, Button, Label } from '@ui5/webcomponents-react'
+import { Dialog, Bar, Button, Label } from '@ui5/webcomponents-react'
 
 import { selectIsImportPopupOpen, sendEmailTemplatesArrayBuffer, setIsImportPopupOpen } from '../../redux/emails/emailSlice'
 import { useSelector, useDispatch } from 'react-redux'
 
 import '@ui5/webcomponents-icons/dist/decline.js'
 import { useState } from 'react'
+
+import './emails-import-popup.component.css'
 
 const EmailsImportPopup = ({ t }) => {
   const dispatch = useDispatch()
@@ -19,7 +21,9 @@ const EmailsImportPopup = ({ t }) => {
   }
 
   const onFileUploaderChangeHandler = (event) => {
-    setImportFile(event.detail.files[0])
+    if (event.detail.files && event.detail.files.lenght > 0) {
+      setImportFile(event.detail.files[0])
+    }
   }
 
   const onCloseEmailImportPopup = () => {
@@ -30,31 +34,88 @@ const EmailsImportPopup = ({ t }) => {
   return (
     <>
       <Dialog
+        className="ui-dialog ui-widget ui-corner-all gigya-jq-dialog"
+        style={{ height: 'auto', width: '400px', top: ' 324px', left: '120px' }}
         open={isImportPopupOpen}
         onAfterClose={onCloseEmailImportPopup}
         id="emailsImportPopup"
-        header={<Bar endContent={<Button id="closeEmailImportPopup" icon="decline" onClick={onCloseEmailImportPopup}></Button>}>{t('EMAILS_IMPORT_POPUP.POPUP_HEADER')}</Bar>}
+        header={
+          <Bar
+            startContent={<div>{t('EMAILS_IMPORT_POPUP.POPUP_HEADER')}</div>}
+            endContent={<Button id="closeEmailImportPopup" icon="decline" onClick={onCloseEmailImportPopup} design="Transparent"></Button>}
+          ></Bar>
+        }
         children={
           <>
             <Label>{t('EMAIL_TEMPLATES_COMPONENT.SPECIFY_FILE')}</Label>
             <br></br>
-            <FileUploader accept=".zip" hideInput onChange={(event) => onFileUploaderChangeHandler(event)}>
-              <Button>{t('EMAIL_TEMPLATES_COMPONENT.CHOOSE_FILE')}</Button>
-            </FileUploader>
-            <Label>{importFile ? importFile.name : t('EMAIL_TEMPLATES_COMPONENT.NO_FILE')}</Label>
+            <input accept=".zip" type={'file'} onChange={(event) => onFileUploaderChangeHandler(event)} />
           </>
         }
         footer={
           <Bar
             endContent={
               <div>
-                <Button onClick={onImportButtonClickHandler}>{t('EMAIL_TEMPLATES_COMPONENT.IMPORT')}</Button>
-                <Button onClick={onCloseEmailImportPopup}>{t('GLOBAL.CANCEL')}</Button>
+                <Button className="btn dialog-button-1" onClick={onImportButtonClickHandler}>
+                  {t('EMAIL_TEMPLATES_COMPONENT.IMPORT')}
+                </Button>
+                <Button className="btn dialog-button-2" onClick={onCloseEmailImportPopup}>
+                  {t('GLOBAL.CANCEL')}
+                </Button>
               </div>
             }
           ></Bar>
         }
       ></Dialog>
+
+      {/* <div
+        className="ui-dialog ui-widget ui-corner-all gigya-jq-dialog"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="ui-dialog-title-2"
+        style={{ height: 'auto', width: '400px', top: ' 324px', left: '120px' }}
+      >
+        <div className="ui-dialog-title-left-corner"></div>
+        <div className="ui-dialog-titlebar" style={{ width: '376px' }}>
+          <span className="ui-dialog-title" id="ui-dialog-title-2">
+            {t('EMAILS_IMPORT_POPUP.POPUP_HEADER')}
+          </span>
+          <span className="ui-dialog-titlebar-close" role="button"></span>
+        </div>
+        <div className="ui-dialog-title-right-corner"></div>
+        <div className="ui-dialog-content-custom" style={{ display: 'block' }}>
+          <gigya-dialog _ngcontent-c11="" scrolltop="0" scrollleft="0" style={{ width: 'auto', height: '99px' }}>
+            <p _ngcontent-c11="">{t('EMAIL_TEMPLATES_COMPONENT.SPECIFY_FILE')}</p>
+            <label _ngcontent-c11="">
+              <div _ngcontent-c11="" class="label">
+                CSV:{' '}
+                <span _ngcontent-c11="" class="red">
+                  *
+                </span>
+              </div>
+              <input _ngcontent-c11="" type="file" />
+            </label>
+            <div class="form-error">&nbsp;</div>
+          </gigya-dialog>
+          <div className="summary-holder">
+            <div className="validation-summary" id="undefinedValidationSummary"></div>
+            <div className="ui-dialog-buttonpane-custom">
+              <div className="ui-dialog-buttonset">
+                <button type="button" className="btn dialog-button-1">
+                  Import
+                </button>
+                <button type="button" className="btn dialog-button-2">
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="clear"></div>
+        </div>
+        <div className="ui-dialog-footer-left-corner"></div>
+        <div className="ui-dialog-footer" style={{ width: '376px' }}></div>
+        <div className="ui-dialog-footer-right-corner"></div>
+      </div> */}
     </>
   )
 }
