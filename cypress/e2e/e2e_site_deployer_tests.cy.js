0eb831bb0a7495f519c65fc5088e04f374b65086
Lopes <g.lopes@sap.com>
2022-12-14 15:45:06 +0000

    CDCTOOLBOX-108: Cypress test on jenkins

--- cypress/e2e/e2e_site_deployer_tests.cy.js
+++ cypress/e2e/e2e_site_deployer_tests.cy.js
@@ -49,7 +49,7 @@ describe('Site Deployer Test Suite', () => {
     writeParentSiteTable(data.parentSiteDomain, data.parentSiteDescription, 2)
     getSaveButton().should('not.be.disabled')
     getSaveButton().click()
-    cy.get('ui5-list').eq(1).should('have.text', data.expectedErrorMessage)
+    cy.get('[class = "MessageItem-title-0-2-59"]').eq(1).should('have.text', data.expectedErrorMessage)
     cy.get('[icon ="error"]').should('be.visible')
   })
 
