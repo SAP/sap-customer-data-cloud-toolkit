922b194229006706e4b92bfd4f64b0a9e49d1d51
Luís Marques <l.marques@sap.com>
2022-12-19 17:10:17 +0000

    bugfix/CDCTOOLBOX-139: Added support to keep visible the main route when sub-routes of that route are shown

--- src/inject/navigation.test.js
+++ src/inject/navigation.test.js
@@ -29,6 +29,14 @@ describe('Navigation test suite', () => {
   test('Process Hash Change Hide container', () => {
     executeTest(`#/${expectedPartnerId}/${expectedApiKey}/mock-different-feature`, false)
   })
+
+  test('Process Hash Change Show container after hidden', () => {
+    executeTest(`#/${expectedPartnerId}/${expectedApiKey}${ROUTE_SITE_DEPLOYER}`, true)
+  })
+
+  test('Process Hash Change Show container when feature is in a sub-level of the main route', () => {
+    executeTest(`#/${expectedPartnerId}/${expectedApiKey}${ROUTE_SITE_DEPLOYER}/sublevel/anothersublevel`, true)
+  })
 })
 
 function executeTest(hash, showClassApplied) {
