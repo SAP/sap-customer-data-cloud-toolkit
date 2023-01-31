bf1db9a7a5391720c9a809dc383327be1dcc9e6e
Bruno Santos <b.santos@sap.com>
2023-01-31 17:29:42 +0000

    CDCTOOLBOX-186:Resolver sonar issues.

--- src/services/copyConfig/info/dataTest.js
+++ src/services/copyConfig/info/dataTest.js
@@ -1,203 +1,210 @@
 import { expectedSchemaResponse } from '../schema/dataTest'
 
 export function getInfoExpectedResponse(supports) {
+  const schema = {
+    id: 'schema',
+    name: 'schema',
+    value: [
+      {
+        id: 'dataSchema',
+        name: 'dataSchema',
+        value: supports,
+      },
+      {
+        id: 'profileSchema',
+        name: 'profileSchema',
+        value: supports,
+      },
+    ],
+  }
+  const screenSets = {
+    id: 'screenSets',
+    name: 'screenSets',
+    value: [
+      {
+        id: 'default',
+        name: 'default',
+        value: [
+          {
+            id: 'defaultLinkAccounts',
+            name: 'defaultLinkAccounts',
+            value: false,
+          },
+          {
+            id: 'defaultLiteRegistration',
+            name: 'defaultLiteRegistration',
+            value: false,
+          },
+        ],
+      },
+      {
+        id: 'custom',
+        name: 'custom',
+        value: [
+          {
+            id: 'customLinkAccounts',
+            name: 'customLinkAccounts',
+            value: false,
+          },
+          {
+            id: 'customLiteRegistration',
+            name: 'customLiteRegistration',
+            value: false,
+          },
+        ],
+      },
+    ],
+  }
+  const policies = {
+    id: 'policies',
+    name: 'policies',
+    value: [
+      {
+        id: 'accountOptions',
+        name: 'accountOptions',
+        value: false,
+      },
+      {
+        id: 'codeVerification',
+        name: 'codeVerification',
+        value: false,
+      },
+      {
+        id: 'emailNotifications',
+        name: 'emailNotifications',
+        value: false,
+      },
+      {
+        id: 'emailVerification',
+        name: 'emailVerification',
+        value: false,
+      },
+      {
+        id: 'federation',
+        name: 'federation',
+        value: false,
+      },
+      {
+        id: 'webSdk',
+        name: 'webSdk',
+        value: false,
+      },
+      {
+        id: 'passwordComplexity',
+        name: 'passwordComplexity',
+        value: false,
+      },
+      {
+        id: 'passwordReset',
+        name: 'passwordReset',
+        value: false,
+      },
+      {
+        id: 'defaultProfilePhotoDimensions',
+        name: 'defaultProfilePhotoDimensions',
+        value: false,
+      },
+      {
+        id: 'registration',
+        name: 'registration',
+        value: false,
+      },
+      {
+        id: 'security',
+        name: 'security',
+        value: false,
+      },
+      {
+        id: 'twoFactorAuthenticationProviders',
+        name: 'twoFactorAuthenticationProviders',
+        value: false,
+      },
+    ],
+  }
+  const socialIdentities = {
+    id: 'socialIdentities',
+    name: 'socialIdentities',
+    value: false,
+  }
+  const emailTemplates = {
+    id: 'emailTemplates',
+    name: 'emailTemplates',
+    value: [
+      {
+        id: 'magicLink',
+        name: 'magicLink',
+        value: false,
+      },
+      {
+        id: 'codeVerification',
+        name: 'codeVerification',
+        value: false,
+      },
+      {
+        id: 'emailVerification',
+        name: 'emailVerification',
+        value: false,
+      },
+      {
+        id: 'newUserWelcome',
+        name: 'newUserWelcome',
+        value: false,
+      },
+      {
+        id: 'accountDeletionConfirmation',
+        name: 'accountDeletionConfirmation',
+        value: false,
+      },
+      {
+        id: 'litePreferencesCenter',
+        name: 'litePreferencesCenter',
+        value: false,
+      },
+      {
+        id: 'doubleOptInConfirmation',
+        name: 'doubleOptInConfirmation',
+        value: false,
+      },
+      {
+        id: 'etPasswordReset',
+        name: 'passwordReset',
+        passwordReset: false,
+      },
+      {
+        id: 'tfaEmailVerification',
+        name: 'tfaEmailVerification',
+        value: false,
+      },
+      {
+        id: 'impossibleTraveler',
+        name: 'impossibleTraveler',
+        value: false,
+      },
+      {
+        id: 'passwordResetConfirmation',
+        name: 'passwordResetConfirmation',
+        value: false,
+      },
+    ],
+  }
+  const smsTemplates = {
+    id: 'smsTemplates',
+    name: 'smsTemplates',
+    value: false,
+  }
+  const dataflows = {
+    id: 'dataflows',
+    name: 'dataflows',
+    value: false,
+  }
   return [
-    {
-      id: 'schema',
-      name: 'schema',
-      value: [
-        {
-          id: 'dataSchema',
-          name: 'dataSchema',
-          value: supports,
-        },
-        {
-          id: 'profileSchema',
-          name: 'profileSchema',
-          value: supports,
-        },
-      ],
-    },
-    {
-      id: 'screenSets',
-      name: 'screenSets',
-      value: [
-        {
-          id: 'default',
-          name: 'default',
-          value: [
-            {
-              id: 'defaultLinkAccounts',
-              name: 'defaultLinkAccounts',
-              value: false,
-            },
-            {
-              id: 'defaultLiteRegistration',
-              name: 'defaultLiteRegistration',
-              value: false,
-            },
-          ],
-        },
-        {
-          id: 'custom',
-          name: 'custom',
-          value: [
-            {
-              id: 'customLinkAccounts',
-              name: 'customLinkAccounts',
-              value: false,
-            },
-            {
-              id: 'customLiteRegistration',
-              name: 'customLiteRegistration',
-              value: false,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 'policies',
-      name: 'policies',
-      value: [
-        {
-          id: 'accountOptions',
-          name: 'accountOptions',
-          value: false,
-        },
-        {
-          id: 'codeVerification',
-          name: 'codeVerification',
-          value: false,
-        },
-        {
-          id: 'emailNotifications',
-          name: 'emailNotifications',
-          value: false,
-        },
-        {
-          id: 'emailVerification',
-          name: 'emailVerification',
-          value: false,
-        },
-        {
-          id: 'federation',
-          name: 'federation',
-          value: false,
-        },
-        {
-          id: 'webSdk',
-          name: 'webSdk',
-          value: false,
-        },
-        {
-          id: 'passwordComplexity',
-          name: 'passwordComplexity',
-          value: false,
-        },
-        {
-          id: 'passwordReset',
-          name: 'passwordReset',
-          value: false,
-        },
-        {
-          id: 'defaultProfilePhotoDimensions',
-          name: 'defaultProfilePhotoDimensions',
-          value: false,
-        },
-        {
-          id: 'registration',
-          name: 'registration',
-          value: false,
-        },
-        {
-          id: 'security',
-          name: 'security',
-          value: false,
-        },
-        {
-          id: 'twoFactorAuthenticationProviders',
-          name: 'twoFactorAuthenticationProviders',
-          value: false,
-        },
-      ],
-    },
-    {
-      id: 'socialIdentities',
-      name: 'socialIdentities',
-      value: false,
-    },
-    {
-      id: 'emailTemplates',
-      name: 'emailTemplates',
-      value: [
-        {
-          id: 'magicLink',
-          name: 'magicLink',
-          value: false,
-        },
-        {
-          id: 'codeVerification',
-          name: 'codeVerification',
-          value: false,
-        },
-        {
-          id: 'emailVerification',
-          name: 'emailVerification',
-          value: false,
-        },
-        {
-          id: 'newUserWelcome',
-          name: 'newUserWelcome',
-          value: false,
-        },
-        {
-          id: 'accountDeletionConfirmation',
-          name: 'accountDeletionConfirmation',
-          value: false,
-        },
-        {
-          id: 'litePreferencesCenter',
-          name: 'litePreferencesCenter',
-          value: false,
-        },
-        {
-          id: 'doubleOptInConfirmation',
-          name: 'doubleOptInConfirmation',
-          value: false,
-        },
-        {
-          id: 'etPasswordReset',
-          name: 'passwordReset',
-          passwordReset: false,
-        },
-        {
-          id: 'tfaEmailVerification',
-          name: 'tfaEmailVerification',
-          value: false,
-        },
-        {
-          id: 'impossibleTraveler',
-          name: 'impossibleTraveler',
-          value: false,
-        },
-        {
-          id: 'passwordResetConfirmation',
-          name: 'passwordResetConfirmation',
-          value: false,
-        },
-      ],
-    },
-    {
-      id: 'smsTemplates',
-      name: 'smsTemplates',
-      value: false,
-    },
-    {
-      id: 'dataflows',
-      name: 'dataflows',
-      value: false,
-    },
+    schema,
+    screenSets,
+    policies,
+    socialIdentities,
+    emailTemplates,
+    smsTemplates,
+    dataflows,
   ]
 }
 
