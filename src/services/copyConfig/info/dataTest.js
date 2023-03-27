7c0fc5cedf2c18a5a8fb40c5ce35078732267031
Bruno Santos <b.santos@sap.com>
2023-03-27 11:53:54 +0100

    CDCTOOLBOX-274:Removed default screenset collection from ScreensetOptions class.

--- src/services/copyConfig/info/dataTest.js
+++ src/services/copyConfig/info/dataTest.js
@@ -1,15 +1,77 @@
 import { expectedSchemaResponse } from '../schema/dataTest'
 import EmailOptions from '../emails/emailOptions'
 import SchemaOptions from '../schema/schemaOptions'
-import ScreenSetOptions from '../screenset/screensetOptions'
 import PolicyOptions from '../policies/policyOptions'
 
 export function getInfoExpectedResponse(supports) {
   const schemaOptions = new SchemaOptions(undefined)
   const schema = supports ? schemaOptions.getOptions() : schemaOptions.getOptionsDisabled()
 
-  const screenSetOptions = new ScreenSetOptions(undefined)
-  const screenSets = supports ? screenSetOptions.getOptions() : screenSetOptions.getOptionsDisabled()
+  const SCREEN_SET_COLLECTION_DEFAULT = 'Default'
+  const screenSets = {
+    id: 'screenSets',
+    name: 'Screen-Sets',
+    value: supports,
+    formatName: false,
+    branches: [
+      {
+        id: SCREEN_SET_COLLECTION_DEFAULT,
+        name: SCREEN_SET_COLLECTION_DEFAULT,
+        value: supports,
+        formatName: false,
+        branches: [
+          {
+            id: `${SCREEN_SET_COLLECTION_DEFAULT}-LinkAccounts`,
+            name: `${SCREEN_SET_COLLECTION_DEFAULT}-LinkAccounts`,
+            formatName: false,
+            value: supports,
+          },
+          {
+            id: `${SCREEN_SET_COLLECTION_DEFAULT}-LiteRegistration`,
+            name: `${SCREEN_SET_COLLECTION_DEFAULT}-LiteRegistration`,
+            formatName: false,
+            value: supports,
+          },
+          {
+            id: `${SCREEN_SET_COLLECTION_DEFAULT}-OrganizationRegistration`,
+            name: `${SCREEN_SET_COLLECTION_DEFAULT}-OrganizationRegistration`,
+            formatName: false,
+            value: supports,
+          },
+          {
+            id: `${SCREEN_SET_COLLECTION_DEFAULT}-PasswordlessLogin`,
+            name: `${SCREEN_SET_COLLECTION_DEFAULT}-PasswordlessLogin`,
+            formatName: false,
+            value: supports,
+          },
+          {
+            id: `${SCREEN_SET_COLLECTION_DEFAULT}-ProfileUpdate`,
+            name: `${SCREEN_SET_COLLECTION_DEFAULT}-ProfileUpdate`,
+            formatName: false,
+            value: supports,
+          },
+          {
+            id: `${SCREEN_SET_COLLECTION_DEFAULT}-ReAuthentication`,
+            name: `${SCREEN_SET_COLLECTION_DEFAULT}-ReAuthentication`,
+            formatName: false,
+            value: supports,
+          },
+          {
+            id: `${SCREEN_SET_COLLECTION_DEFAULT}-RegistrationLogin`,
+            name: `${SCREEN_SET_COLLECTION_DEFAULT}-RegistrationLogin`,
+            formatName: false,
+            value: supports,
+          },
+          {
+            id: `${SCREEN_SET_COLLECTION_DEFAULT}-Subscriptions`,
+            name: `${SCREEN_SET_COLLECTION_DEFAULT}-Subscriptions`,
+            formatName: false,
+            value: supports,
+          },
+        ],
+      },
+    ],
+  }
 
   const policiesOptions = new PolicyOptions(undefined)
   const policies = supports ? policiesOptions.getOptions() : policiesOptions.getOptionsDisabled()
