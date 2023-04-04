ce7f32bc4ecf09a1cbbc26194912899ebd248419
Guilherme Lopes <g.lopes@sap.com>
2023-04-04 19:38:02 +0100

    Feature/cdctoolbox 249 (#171)
    * CDCTOOLBOX-270: Copy All Configs
    
    * CDCTOOLBOX-270: Copy All Configs
    
    * CDCTOOLBOX-270:Implementing wait until plugin
    
    * CDCTOOLBOX-270: Implementing waitUntil plugin and schema Validation
    
    * CDCTOOLBOX-270:All sintax errors resolved
    
    * CDCTOOLBOX-270:merge conflicts resolved
    
    * CDCTOOLBOX-270:uncommented
    
    * CDCTOOLBOX-270:uncommented
    
    * CDCTOOLBOX-270:Social Providers finished
    
    * CDCTOOLBOX-270: Remove unused imports
    
    * CDCTOOLBOX-270: Refactor code
    
    * CDCTOOLBOX-249: Validate Social Login

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     passWord: `${process.env.passWord}`,
     codeCoverageTasksRegistered: true,
   },
-  // defaultCommandTimeout: 30000,
+  defaultCommandTimeout: 30000,
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
