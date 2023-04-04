fd98f6a2e355743f44157b2f3ca9e3fb7b7a4fb8
Bruno Santos <b.santos@sap.com>
2023-04-04 19:04:43 +0100

    CDCTOOLBOX-277:Reverted cypress/e2e/e2eFullTesting.cy.js

--- cypress/e2e/e2eFullTesting.cy.js
+++ cypress/e2e/e2eFullTesting.cy.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-undef */
 import * as utils from './utils'
 import * as dataTest from './dataTest'
-import { single } from 'rxjs'
-import { func } from 'prop-types'
 
 describe('All features full Test Suite', () => {
   it('All features tests', () => {
@@ -14,9 +12,9 @@ describe('All features full Test Suite', () => {
     getSelectedOption(dataTest.siteDeployerIconName)
 
     testSiteDeployer(dataTest.baseDomainName)
-    // // Navigating to the Site that was created
+    // // // Navigating to the Site that was created
     navigateToChosenSite(dataTest.baseDomainName)
-    // // Email export and import use cases
+    // Email export and import use cases
     getSelectedOption(dataTest.emailTemplatesIconName)
     testImportExportEmailTemplatesFirstUseCase()
     testImportExportEmailTemplatesSecondCase()
@@ -28,16 +26,20 @@ describe('All features full Test Suite', () => {
     getSelectedOption(dataTest.smsTemplatesOption)
     testImportExportSmsFirstUseCaseTemplates()
     testImportExportSmsSecondUseCaseTemplates()
-    //Copy configurations to test site
-
+    //Copy Web Sdk Testing use Case
+    //  - Copy Schema
+    //  - Copy Screen Sets
+    //  - Copy Policies
+    //  - Copy Social Identities
+    //  - Copy Email Templates
+    //  - Copy Sms Templates
+    //  - Copy Dataflows
+    //  - Copy Web Sdk
     navigateToChosenSite(dataTest.templateSiteName)
     const targetSites = [dataTest.targetSiteDomainName, dataTest.target2SiteDomainName]
     copyConfigTesting(targetSites)
-    // // Navigating to the Site that was altered
-    // //Change to the desired site and check the changes
-    navigateToChosenSite(dataTest.baseDomainName)
-    targetSites.forEach(validateChanges)
-    // // Delete the site created on this test
+    targetSites.forEach(validateCopiedConfigs)
+    // // // // Delete the site created on this test
     getSelectedOption(dataTest.siteSelectorOption)
     deleteSiteCreated()
   })
@@ -55,28 +57,24 @@ describe('All features full Test Suite', () => {
 
     utils.getSaveButton().click()
 
-    cy.waitUntil(() => cy.get('#successPopup').then((win) => cy.get(win).should('be.visible')))
-
     cy.get('#successPopup').shadow().find('[id="ui5-popup-header"]').should('have.text', dataTest.successMessageHeader)
     utils.clickPopUpOkButton('#successPopup')
-    cy.window().its('open').should('be.called')
+    cy.wait(10000)
   }
   function copyConfigTesting(targetSites) {
+    //Copy Web SDK to desired site
     getSelectedOption(dataTest.copyConfigExtendendMenuOption)
     cy.get('#currentSiteName').should('have.text', dataTest.templateSiteName)
     targetSites.forEach(addSiteToTargetList)
     cy.get('ui5-list').find('ui5-li-custom').should('have.length', targetSites.length)
     cy.get('#selectAllCheckbox').click()
-
     cy.get('#saveButton').click()
-    cy.waitUntil(() => cy.get('#copyConfigSuccessPopup').then((win) => cy.get(win).should('be.visible')))
-
     cy.get('#copyConfigSuccessPopup').shadow().find('[id="ui5-popup-header"]').should('have.text', dataTest.successMessageHeader)
-
     cy.get('#copyConfigSuccessPopup').find('ui5-bar').find('[id="closeButton"]').click()
-    cy.window().its('open').should('be.called')
+    cy.get('#copyConfigSuccessPopup').should('not.be.visible')
   }
-  function addSiteToTargetList(target) {
+
+  function addSiteToTargetList(target){
     cy.get('input').first().focus()
     cy.get('#targetApiKeyInput').shadow().find('[class="ui5-input-inner"]').type(target)
     cy.get('#targetApiKeyInput').shadow().find('[class="ui5-input-inner"]').should('have.value', target)
@@ -84,16 +82,34 @@ describe('All features full Test Suite', () => {
     cy.get('ui5-static-area-item').shadow().find('ui5-list').find('ui5-li-suggestion-item').eq(0).click()
   }
 
+  function validateCopiedConfigs(baseDomainName) {
+    // // // // Navigating to the Site that was altered
+    // // // //Change to the desired site and check the changes
+    navigateToChosenSite(baseDomainName)
+    // // // //Change to web sdk and check the changes
+    getSelectedOption(dataTest.webSDKConfiguration)
+    cy.get('main-app').shadow().find('web-sdk-configuration-app').shadow().find('web-sdk-configuration-container').find('fd-layout-panel').eq(0).contains(dataTest.webSdkCopyTest)
+    // Check email template changes
+    getSelectedOption(dataTest.emailTemplatesIconName)
+    cy.get('main-app').shadow().find('email-templates-web-app').shadow().find('languages-list').find('[class="locales-item__name"]').should('have.length', '5')
+
+    // // // //Check sms template changes
+    getSelectedOption(dataTest.smsTemplatesIconName)
+    cy.get('main-app').shadow().find('sms-templates-web-app').shadow().find('[class="fd-tabs__item"]').eq(0).should('be.visible')
+    cy.get('main-app').shadow().find('sms-templates-web-app').shadow().find('[class="fd-tabs__item"]').eq(0).should('have.text', 'TFA')
+    cy.get('main-app').shadow().find('sms-templates-web-app').shadow().find('[class="languages_list_container"]')
+        .eq(1).find('[role="list"]')
+        .should('have.text', dataTest.templateSiteNameSmsTemplatesContent)
+  }
+
   function testImportExportEmailTemplatesFirstUseCase() {
     // Email Templates -  First Use Case
     // Exporting and Importing the original template
 
-    cy.waitUntil(() => cy.get('#exportAllEmailTemplatesButton').then((win) => cy.get(win).should('be.visible')))
     cy.get('#exportAllEmailTemplatesButton').click({ force: true })
 
     cy.get('#importAllEmailTemplatesButton').click({ force: true })
-    cy.waitUntil(() => cy.get('#emailsImportPopup').then((win) => cy.get(win).should('be.visible')))
-
+    cy.get('#confirmButton').click({ force: true })
     cy.get('#zipFileInput').selectFile(`${dataTest.cypressDownloadsPath}${dataTest.emailExampleFile}`, { force: true })
     cy.get('#emailsImportPopup').find('[id ="importZipButton"]').click()
     cy.get('#confirmButton').click({ force: true })
@@ -101,42 +117,24 @@ describe('All features full Test Suite', () => {
     cy.get('main-app').shadow().find('email-templates-web-app').shadow().find('fd-card-content').find('[class="fd-popover__control"]').eq(0).click({ force: true })
     cy.get('.cdk-overlay-container').find('fd-option').eq(4).click()
     cy.get('main-app').shadow().find('email-templates-web-app').shadow().find('languages-list').find('[class="locales-item__name"]').should('have.length', '44')
-    cy.get('#emailTemplatesErrorPopup').should('be.visible')
-    cy.get('#emailTemplatesErrorPopup').find('#closeButton').click({ force: true })
+    getSelectedOption(dataTest.emailTemplatesIconName)
     utils.clickPopUpOkButton('#successPopup')
-    cy.waitUntil(() => cy.get('#exportAllEmailTemplatesButton').then((win) => cy.get(win).should('be.visible')))
-    cy.window().its('open').should('be.called')
+    cy.wait(10000)
   }
   function testImportExportEmailTemplatesSecondCase() {
     //Email Templates - Second Use Case
     //Importing the test template with added languages
 
     cy.get('#importAllEmailTemplatesButton').click({ force: true })
-    cy.waitUntil(() => cy.get('#emailsImportPopup').then((win) => cy.get(win).should('be.visible')))
-
     cy.get('#zipFileInput').attachFile(dataTest.emailExampleFile, { force: true })
     cy.get('#emailsImportPopup').find('[id ="importZipButton"]').click()
     cy.get('#confirmButton').click({ force: true })
     utils.clickPopUpOkButton('#successPopup')
-    cy.waitUntil(() =>
-      cy
-        .get('main-app')
-        .shadow()
-        .find('email-templates-web-app')
-        .then((win) => cy.get(win).should('be.visible'))
-    )
-
     cy.get('main-app').shadow().find('email-templates-web-app').shadow().find('fd-card-content').find('[class="fd-popover__control"]').eq(0).click({ force: true })
-    cy.waitUntil(() =>
-      cy
-        .get('.cdk-overlay-container')
-        .find('fd-option')
-        .then((win) => cy.get(win).should('be.visible'))
-    )
     cy.get('.cdk-overlay-container').find('fd-option').eq(0).click()
     cy.get('main-app').shadow().find('email-templates-web-app').shadow().find('languages-list').should('be.visible')
-    cy.get('main-app').shadow().find('email-templates-web-app').shadow().find('languages-list').find('[class="locales-item__name"]').should('have.length', '6')
-    cy.window().its('open').should('be.called')
+    cy.get('main-app').shadow().find('email-templates-web-app').shadow().find('languages-list').find('[class="locales-item__name"]').should('have.length', '2')
+    cy.wait(10000)
   }
   function testImportExportEmailTemplatesThirdCase() {
     // Email Templates - Third Use Case
@@ -153,24 +151,16 @@ describe('All features full Test Suite', () => {
     cy.get('#emailTemplatesErrorPopup').should('be.visible')
     cy.get('#emailTemplatesErrorPopup').find('#closeButton').click({ force: true })
     cy.get('body').find('#openPopoverButton').click({ force: true })
-    cy.waitUntil(() =>
-      cy
-        .get('#credentialsResponsivePopover')
-
-        .then((win) => cy.get(win).should('be.visible'))
-    )
-
     cy.get('#userKey').shadow().find('[class = "ui5-input-inner"]').focus().type('{backspace}', { force: true })
-    cy.get('#emailTemplatesErrorPopup').find('#closeButton').click({ force: true })
-
+    cy.wait(1000)
     cy.get('body').find('#openPopoverButton').click({ force: true })
-    cy.window().its('open').should('not.be.called')
+    cy.get('#emailTemplatesErrorPopup').find('#closeButton').click({ force: true })
+    cy.wait(10000)
   }
 
   function testImportExportSmsFirstUseCaseTemplates() {
     // SMS Templates - First Use Case
     // Exporting and Importing the original template
-    cy.waitUntil(() => cy.get('#exportAllSmsTemplatesButton').then((win) => cy.get(win).should('be.visible')))
 
     cy.get('#exportAllSmsTemplatesButton').click({ force: true })
     cy.get('#importAllSmsTemplatesButton').click({ force: true })
@@ -191,14 +181,13 @@ describe('All features full Test Suite', () => {
       .find('[class="fd-list__title"]')
       .click({ force: true })
     cy.get('main-app').shadow().find('sms-templates-web-app').shadow().find('[class="langauge-item"]').should('have.length', '40')
-    cy.window().its('open').should('be.called')
+    cy.wait(10000)
   }
   function testImportExportSmsSecondUseCaseTemplates() {
     // SMS Templates - Second Use Case
     // Importing and validating the template with new changes
 
     cy.get('#importAllSmsTemplatesButton').click({ force: true })
-    cy.waitUntil(() => cy.get('#smsImportPopup').then((win) => cy.get(win).should('be.visible')))
     cy.get('#zipFileInput').attachFile(dataTest.smsExampleFile)
     cy.get('#importZipButton').click({ force: true })
     cy.get('#smsTemplatesErrorPopup').should('be.visible')
@@ -217,23 +206,22 @@ describe('All features full Test Suite', () => {
       .find('[class="fd-list__title"]')
       .click({ force: true })
     cy.get('main-app').shadow().find('sms-templates-web-app').shadow().find('[class="langauge-item"]').should('have.length', '40')
-    cy.window().its('open').should('be.called')
+    cy.wait(10000)
   }
 
   function navigateToChosenSite(siteName) {
     getSelectedOption(dataTest.siteSelectorOption)
+    cy.get('main-app')
+      .shadow()
+      .find('site-selector-web-app')
+      .shadow()
+      .find('[class ="fd-section app__header"]')
+      .find('[class ="fd-input-group"]')
+      .find('[placeholder="Search"]')
+      .click()
+      .clear({ force: true })
+      .type(siteName)
 
-    cy.waitUntil(() =>
-      cy
-        .get('main-app')
-        .shadow()
-        .find('site-selector-web-app')
-        .shadow()
-        .find('[class ="fd-section app__header"]')
-        .find('[class ="fd-input-group"]')
-        .find('[placeholder="Search"]')
-        .then((input) => cy.get(input).should('not.be.disabled').clear().type(siteName, { force: true }))
-    )
     cy.get('main-app')
       .shadow()
       .find('[class ="app-area"]')
@@ -243,6 +231,7 @@ describe('All features full Test Suite', () => {
       .find('[class ="fd-link base-domain"]')
       .eq(0)
       .click()
+    cy.wait(10000)
   }
   function deleteSiteCreated() {
     getSelectedOption(dataTest.siteSelectorOption)
@@ -251,7 +240,7 @@ describe('All features full Test Suite', () => {
       .find('[class ="app-area"]')
       .find('site-selector-web-app')
       .shadow()
-      .find('[class ="fd-form-item"]')
+      .find('[class ="fd-table__body"]')
       .find('[class="fd-table__cell"]')
       .eq(3)
       .find('sslct-site-actions')
@@ -268,76 +257,10 @@ describe('All features full Test Suite', () => {
     cy.get('main-app').shadow().find('[class ="fd-nested-list__item"]').contains(optionName).click({ force: true })
   }
   function loginToGigya(URL) {
-    cy.visit('https://' + URL, {
-      onBeforeLoad(win) {
-        cy.stub(win, 'open')
-      },
-    })
+    cy.visit('https://' + URL)
 
     cy.get('[name ="username"]', { timeout: 10000 }).eq(2).type(Cypress.env('userName'))
     cy.get('[name="password"]').eq(3).type(Cypress.env('passWord'))
     cy.get('[class = "gigya-input-submit"]').eq(8).click()
-  }
-
-  function validateChanges() {
-    //Copy Web Sdk Testing use Case
-    //  - Copy Schema
-    //  - Copy Screen Sets
-    //  - Copy Policies
-    //  - Copy Social Identities
-    //  - Copy Email Templates
-    //  - Copy Sms Templates
-    //  - Copy Dataflows
-    //  - Copy Web Sdk
-
-    //Check Schema Options
-    checkSAccountsSchema()
-    // Check email template changes
-    checkEmailTemplates()
-    // Check sms template changes
-    checkSmsTemplates()
-    // Change to web sdk and check the changes
-    checkWebSdk()
-  }
-  function checkWebSdk() {
-    getSelectedOption(dataTest.webSDKConfiguration)
-    cy.get('main-app').shadow().find('web-sdk-configuration-app').shadow().find('web-sdk-configuration-container').find('fd-layout-panel').eq(0).contains(dataTest.webSdkCopyTest)
-  }
-  function checkEmailTemplates() {
-    getSelectedOption(dataTest.emailTemplatesIconName)
-    cy.get('main-app').shadow().find('email-templates-web-app').shadow().find('languages-list').find('[class="locales-item__name"]').should('have.length', '6')
-  }
-  function checkSmsTemplates() {
-    getSelectedOption(dataTest.smsTemplatesIconName)
-    cy.get('main-app').shadow().find('sms-templates-web-app').shadow().find('[class="fd-tabs__item"]').eq(0).should('be.visible')
-    cy.get('main-app').shadow().find('sms-templates-web-app').shadow().find('[class="fd-tabs__item"]').eq(0).should('have.text', 'TFA')
-    cy.get('main-app')
-      .shadow()
-      .find('sms-templates-web-app')
-      .shadow()
-      .find('[class="languages_list_container"]')
-      .eq(1)
-      .find('[role="list"]')
-
-      .should('have.text', dataTest.templateSiteNameSmsTemplatesContent)
-  }
-  function checkSAccountsSchema() {
-    getSelectedOption(dataTest.accountsSchemaOption)
-    cy.get('main-app')
-      .shadow()
-      .find('schema-editor-web-app')
-      .shadow()
-      .find('fd-layout-panel-body')
-      .find('[class="fd-tree__row"]')
-      .eq(1)
-      .should('have.text', dataTest.schemadataTestFieldOne)
-    cy.get('main-app')
-      .shadow()
-      .find('schema-editor-web-app')
-      .shadow()
-      .find('fd-layout-panel-body')
-      .find('[class="fd-tree__row"]')
-      .eq(2)
-      .should('have.text', dataTest.schemadataTestFieldTwo)
   }
 })
