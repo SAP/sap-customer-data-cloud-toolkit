e0c144fc14b696a718a510a237c4f6afb0709b3e
Bruno Santos <b.santos@sap.com>
2023-07-31 19:57:01 +0100

    CDCTOOLBOX-349: Update links of the release to the new repo

--- src/services/github/gitHubManager.js
+++ src/services/github/gitHubManager.js
@@ -11,18 +11,16 @@ import { VERSION } from '../../constants'
 class GitHubManager {
   static ERROR_MSG_RELEASE = 'Error accessing release information on git hub'
   #protocol = 'https'
-  #github = 'github.tools.sap'
-  #api = 'api/v3'
+  #github = 'api.github.com'
 
   constructor() {
-    this.gitHubClient = new GitHubClient(`${this.#protocol}://${this.#github}/${this.#api}`, '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') // repo_deployment permission only
+    this.gitHubClient = new GitHubClient(`${this.#protocol}://${this.#github}`, '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') // repo_deployment permission only
   }
 
   async getNewReleaseAvailable() {
     const latestReleaseInformation = await this.gitHubClient.getLatestReleaseInformation().catch(function (error) {
       return Promise.reject(generateErrorResponse(error, GitHubManager.ERROR_MSG_RELEASE).data)
     })
-    this.latestReleaseInformation = latestReleaseInformation.data
     const latestReleaseVersion = latestReleaseInformation.data.tag_name
     const currentReleaseVersion = VERSION
     return {
