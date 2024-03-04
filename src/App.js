015b5fa097e9b97efc8ccb6f19fc72d3543b4394
Lopes <g.lopes@sap.com>
2024-03-04 16:06:58 +0000

    CDCTOOLBOX-377-Installing theme

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@
  * License: Apache-2.0
  */
 
-
 /*
  * Copyright: Copyright 2023 SAP SE or an SAP affiliate company and cdc-tools-chrome-extension contributors
  * License: Apache-2.0
  */
-
 
 import { ThemeProvider } from '@ui5/webcomponents-react'
 
@@ -17,12 +15,14 @@ import EmailTemplates from './routes/email-templates/email-templates.component'
 import SmsTemplates from './routes/sms-templates/sms-templates.component'
 import CopyConfigurationExtended from './routes/copy-configuration-extended/copy-configuration-extended.component'
 import CredentialsPopoverButton from './components/credentials-popover-button/credentials-popover-button.component'
-
+import { setTheme } from '@ui5/webcomponents-base/dist/config/Theme'
+import '@ui5/webcomponents-react/dist/Assets'
 import { ROUTE_CONTAINER_CLASS, ROUTE_SITE_DEPLOYER, ROUTE_EMAIL_TEMPLATES, ROUTE_SMS_TEMPLATES, ROUTE_COPY_CONFIG_EXTENDED, TOPBAR_MENU_CONTAINER_CLASS } from './inject/constants'
 
 import './App.css'
 
 function App() {
+  setTheme('sap_horizon')
   return (
     <ThemeProvider>
       <div className="App">
--- src/services/copyConfig/options.js
+++ src/services/copyConfig/options.js
@@ -3,7 +3,6 @@
  * License: Apache-2.0
  */
 
-
 class Options {
   constructor(options) {
     this.options = options
