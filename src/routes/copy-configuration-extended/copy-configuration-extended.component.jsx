5f3992c6176d7037b6e9ded32452ec71ca64069b
Tiago Gaspar <tiago.gaspar@sap.com>
2024-03-27 11:34:29 +0000

    feature/CDCTOOLBOX-386 - resolve sonar issues on routes

--- src/routes/copy-configuration-extended/copy-configuration-extended.component.jsx
+++ src/routes/copy-configuration-extended/copy-configuration-extended.component.jsx
@@ -168,27 +168,30 @@ const CopyConfigurationExtended = ({ t }) => {
     })
   }
 
-  const onSelectAllIncludeUrlChangeHandler = (event) => {
-    let checkBoxId
-    configurations.forEach((configuration) => {
-      if (configuration.branches && configuration.branches.length > 0) {
-        configuration.branches.forEach((branch) => {
-          if (branch.name && branch.name.includes("Include")) {
-            checkBoxId = branch.id
-            dispatch(setConfigurationStatus({ checkBoxId, value:false }))
-          }
-          if (branch.branches && branch.branches.length > 0) {
-            branch.branches.forEach((nestedBranch) => {
-              if (nestedBranch.name && nestedBranch.name.includes("Include")) {
-                 checkBoxId = nestedBranch.id
-                dispatch(setConfigurationStatus({ checkBoxId, value:false }))
-              }
-            });
-          }
-        });
-      }
-    });
-  };
+const handleCheckboxChange = (checkbox, value) => {
+  if (checkbox.name && checkbox.name.includes("Include")) {
+    const checkBoxId = checkbox.id;
+    dispatch(setConfigurationStatus({ checkBoxId, value: value }));
+  }
+};
+
+const processNestedBranches = (branches, value) => {
+  branches.forEach(branch => {
+    handleCheckboxChange(branch, value);
+
+    if (branch.branches && branch.branches.length > 0) {
+      branch.branches.forEach(nestedBranch => handleCheckboxChange(nestedBranch, value));
+    }
+  });
+};
+
+const onSelectAllIncludeUrlChangeHandler = (event) => {
+  configurations.forEach((configuration) => {
+    if (configuration.branches && configuration.branches.length > 0) {
+      processNestedBranches(configuration.branches, false);
+    }
+  });
+};
 
   const showSuccessMessage = () => (
     <DialogMessageInform
