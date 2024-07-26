70442a29569a75a5c767ee5a5e587e62c5703221
Luís Marques <l.marques@sap.com>
2024-07-26 15:24:24 +0100

    CDCTOOLBOX-408: Update path for tracking tool theme

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ import CopyConfigurationExtended from './routes/copy-configuration-extended/copy
 import EmailTemplates from './routes/email-templates/email-templates.component'
 import SiteDeployer from './routes/site-deployer/site-deployer.component'
 import SmsTemplates from './routes/sms-templates/sms-templates.component'
-import '@sap_oss/automated-usage-tracking-tool/styles/sap_horizon.css'
+
+import '@sap_oss/automated-usage-tracking-tool/theme/sap_horizon.css'
 import { requestConsentConfirmation } from './lib/tracker'
 
 function App() {
