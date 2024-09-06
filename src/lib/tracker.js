25574a5dac868f0c7f62a4f909c67a0472f13c1d
Lara Langnau <lara.langnau@sap.com>
2024-09-06 11:22:39 -0400

    CDCTOOLBOX-408: change initialization of tracker

--- src/lib/tracker.js
+++ src/lib/tracker.js
@@ -3,23 +3,35 @@ import Web from '@sap_oss/automated-usage-tracking-tool'
 const API_KEY = process.env.REACT_APP_RELEASE_BUILD ? process.env.REACT_APP_TRACKER_API_KEY_PROD : process.env.REACT_APP_TRACKER_API_KEY_DEV
 const DATA_CENTER = process.env.REACT_APP_RELEASE_BUILD ? process.env.REACT_APP_TRACKER_DATA_CENTER_PROD : process.env.REACT_APP_TRACKER_DATA_CENTER_DEV
 
-const trackingTool = new Web({
-  apiKey: API_KEY,
-  dataCenter: DATA_CENTER,
-})
+let trackingTool = null
+
+if (API_KEY && DATA_CENTER) {
+  trackingTool = new Web({
+    apiKey: API_KEY,
+    dataCenter: DATA_CENTER,
+  })
+} else {
+  console.log('Tracking tool was not initialized due to missing API key or data center configuration.')
+}
 
 export async function requestConsentConfirmation() {
-  return await trackingTool.requestConsentConfirmation({
-    message: `
+  if (trackingTool) {
+    return await trackingTool.requestConsentConfirmation({
+      message: `
       <h2>SAP Customer Data Cloud toolkit</h2>
       This app collects anonymous usage data to help deliver and improve this product. By installing this app, you agree to share this information with SAP. If you wish to revoke your consent, please uninstall the app. Do you want to continue?
     `,
-  })
+    })
+  }
+  return null
 }
 
 export async function trackUsage({ featureName }) {
-  return await trackingTool.trackUsage({
-    toolName: 'sap-customer-data-cloud-toolkit',
-    featureName,
-  })
+  if (trackingTool) {
+    return await trackingTool.trackUsage({
+      toolName: 'sap-customer-data-cloud-toolkit',
+      featureName,
+    })
+  }
+  return null
 }
