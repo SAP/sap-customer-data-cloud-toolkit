1d6f73313003583a3f4eb6c00d2c302525e225d4
Lopes <g.lopes@sap.com>
2024-09-09 13:54:59 +0100

    Changing prettier

--- src/services/prettierValidator/prettierFunction.js
+++ src/services/prettierValidator/prettierFunction.js
@@ -5,14 +5,14 @@
 import { format } from 'prettier/standalone'
 import * as prettierPluginBabel from 'prettier/plugins/babel'
 import * as prettierPluginEstree from 'prettier/plugins/estree'
-import { stringToJson } from '../copyConfig/objectHelper.js'
+// import { stringToJson } from '../copyConfig/objectHelper.js'
 import UrlBuilder from '../gigya/urlBuilder.js'
 import generateErrorResponse from '../errors/generateErrorResponse.js'
 import client from '../gigya/client.js'
 
 class StringPrettierFormatter {
   static #ERROR_MSG_GET_CONFIG = 'Error getting screen sets'
-  static #ERROR_MSG_SET_CONFIG = 'Error setting screen sets'
+  // static #ERROR_MSG_SET_CONFIG = 'Error setting screen sets'
   static #NAMESPACE = 'accounts'
   #credentials
   #site
@@ -33,25 +33,25 @@ class StringPrettierFormatter {
     return res.data
   }
 
-  async set(site, dataCenter, body) {
-    const url = UrlBuilder.buildUrl(StringPrettierFormatter.#NAMESPACE, dataCenter, StringPrettierFormatter.getSetScreenSetEndpoint(), this.#credentials.gigyaConsole)
-    const res = await client.post(url, this.#setScreenSetParameters(site, body)).catch(function (error) {
-      return generateErrorResponse(error, StringPrettierFormatter.#ERROR_MSG_SET_CONFIG)
-    })
+  // async set(site, dataCenter, body) {
+  //   const url = UrlBuilder.buildUrl(StringPrettierFormatter.#NAMESPACE, dataCenter, StringPrettierFormatter.getSetScreenSetEndpoint(), this.#credentials.gigyaConsole)
+  //   const res = await client.post(url, this.#setScreenSetParameters(site, body)).catch(function (error) {
+  //     return generateErrorResponse(error, StringPrettierFormatter.#ERROR_MSG_SET_CONFIG)
+  //   })
 
-    return res.data
-  }
+  //   return res.data
+  // }
 
-  async copy(destinationSite, destinationSiteConfiguration, options) {
-    let response = await this.get()
+  // async copy(destinationSite, destinationSiteConfiguration, options) {
+  //   let response = await this.get()
 
-    if (response.errorCode === 0) {
-      response = await this.copyScreenSets(destinationSite, destinationSiteConfiguration.dataCenter, response, options)
-    }
-    stringToJson(response, 'context')
+  //   if (response.errorCode === 0) {
+  //     response = await this.copyScreenSets(destinationSite, destinationSiteConfiguration.dataCenter, response, options)
+  //   }
+  //   stringToJson(response, 'context')
 
-    return response
-  }
+  //   return response
+  // }
 
   async specificScreenSet(specificScreenSet, siteApiKey, javascript, response) {
     const screenSetArray = []
@@ -134,50 +134,50 @@ class StringPrettierFormatter {
     return parameters
   }
 
-  #setScreenSetParameters(apiKey, body) {
-    const parameters = Object.assign({})
-    parameters.apiKey = apiKey
-    parameters.userKey = this.#credentials.userKey
-    parameters.secret = this.#credentials.secret
-    parameters['screenSetID'] = body.screenSetID
-    parameters['html'] = body.html
-    if (body.css) {
-      parameters['css'] = body.css
-    }
-    if (body.javascript) {
-      parameters['javascript'] = body.javascript
-    }
-    if (body.translations) {
-      parameters['translations'] = JSON.stringify(body.translations)
-    }
-    if (body.metadata) {
-      parameters['metadata'] = JSON.stringify(body.metadata)
-    }
-    parameters['context'] = JSON.stringify({ id: body.screenSetID, targetApiKey: apiKey })
-    return parameters
-  }
+  // #setScreenSetParameters(apiKey, body) {
+  //   const parameters = Object.assign({})
+  //   parameters.apiKey = apiKey
+  //   parameters.userKey = this.#credentials.userKey
+  //   parameters.secret = this.#credentials.secret
+  //   parameters['screenSetID'] = body.screenSetID
+  //   parameters['html'] = body.html
+  //   if (body.css) {
+  //     parameters['css'] = body.css
+  //   }
+  //   if (body.javascript) {
+  //     parameters['javascript'] = body.javascript
+  //   }
+  //   if (body.translations) {
+  //     parameters['translations'] = JSON.stringify(body.translations)
+  //   }
+  //   if (body.metadata) {
+  //     parameters['metadata'] = JSON.stringify(body.metadata)
+  //   }
+  //   parameters['context'] = JSON.stringify({ id: body.screenSetID, targetApiKey: apiKey })
+  //   return parameters
+  // }
 
   static getGetScreenSetEndpoint() {
     return `${StringPrettierFormatter.#NAMESPACE}.getScreenSets`
   }
 
-  static getSetScreenSetEndpoint() {
-    return `${StringPrettierFormatter.#NAMESPACE}.setScreenSet`
-  }
+  // static getSetScreenSetEndpoint() {
+  //   return `${StringPrettierFormatter.#NAMESPACE}.setScreenSet`
+  // }
 
-  async copyScreenSets(destinationSite, dataCenter, response, options) {
-    const promises = []
-    for (const screenSetCollectionInfo of options.getOptions().branches) {
-      for (const screenSetInfo of screenSetCollectionInfo.branches) {
-        if (screenSetInfo.value) {
-          const index = response.screenSets.findIndex((arrayId) => arrayId.screenSetID === screenSetInfo.id)
-          response.screenSets[index].javascript = await this.myFormat(response.screenSets[index].javascript)
-          promises.push(this.#copyScreenSet(destinationSite, screenSetInfo.name, dataCenter, response))
-        }
-      }
-    }
-    return Promise.all(promises)
-  }
+  // async copyScreenSets(destinationSite, dataCenter, response, options) {
+  //   const promises = []
+  //   for (const screenSetCollectionInfo of options.getOptions().branches) {
+  //     for (const screenSetInfo of screenSetCollectionInfo.branches) {
+  //       if (screenSetInfo.value) {
+  //         const index = response.screenSets.findIndex((arrayId) => arrayId.screenSetID === screenSetInfo.id)
+  //         response.screenSets[index].javascript = await this.myFormat(response.screenSets[index].javascript)
+  //         promises.push(this.#copyScreenSet(destinationSite, screenSetInfo.name, dataCenter, response))
+  //       }
+  //     }
+  //   }
+  //   return Promise.all(promises)
+  // }
 
   #copyScreenSet(destinationSite, screenSetID, dataCenter, response) {
     const screenSet = this.#getScreenSet(screenSetID, response)
