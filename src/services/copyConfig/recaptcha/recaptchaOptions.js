a1cf0d6da4f908122b0983ac4e62c0e969a33b0a
Ricardo Gaiao <ricardo.gaiao@sap.com>
2024-09-11 09:52:56 +0100

    CDCTOOLBOX-412: Name Correction

--- src/services/copyConfig/recaptcha/recaptchaOptions.js
+++ src/services/copyConfig/recaptcha/recaptchaOptions.js
@@ -1,7 +1,7 @@
 /*
  * Copyright: Copyright 2023 SAP SE or an SAP affiliate company and cdc-tools-chrome-extension contributors
  * License: Apache-2.0
- */ 
+ */
 
 import Options from '../options.js'
 
@@ -11,7 +11,7 @@ class RecaptchaOptions extends Options {
   constructor(recaptchaConfiguration) {
     super({
       id: 'recaptchaPolicies',
-      name: 'reCAPTCHA Policies',
+      name: 'CAPTCHA Policies',
       formatName: false,
       value: true,
     })
