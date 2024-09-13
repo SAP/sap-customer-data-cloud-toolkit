2f9f8d0f9909a7c78ce8933a8c0fa045206a23d7
Lopes <g.lopes@sap.com>
2024-09-13 11:37:35 +0100

    Adding codemirror helper

--- src/routes/codeLinter/codeLinter.component.jsx
+++ src/routes/codeLinter/codeLinter.component.jsx
@@ -7,64 +7,49 @@ import { withTranslation } from 'react-i18next'
 
 import React from 'react'
 
-// import Code from '../../codeMirror'
 import { Bar, Button } from '@ui5/webcomponents-react'
 import { createUseStyles } from 'react-jss'
 import styles from './email-templates.styles.js'
-import CodeLinter from '../../services/linter/eslint.js'
+import '../../services/linter/eslint.js'
+import CodeMirror from 'codemirror'
 const CodeMirrorEditor = () => {
-  const linter = new CodeLinter()
-  console.log('linter', linter.codeLinter())
+  let code = `export default {
+    // Called when an error occurs.
+    onError: function(event) {
+   a + b
+    },
+};
+`
+  const mainApp = document.querySelector('main-app')
+
+  const shadowRoot = mainApp.shadowRoot
+  const screenSet = shadowRoot.querySelector('app-root')
+  const iframe = screenSet.querySelector('iframe.console-app-iframe')
+
+  const iframeDocument = iframe.contentDocument || iframe.contentWindow.document
+  if (!iframeDocument) {
+    console.log("iframe couldn't be found in DOM.")
+    return
+  }
+
+  const mainAppIframe = iframeDocument.querySelectorAll('#screenSet')
+  console.log('Original mainAppIframe:', mainAppIframe)
+  const playground = mainAppIframe[0].querySelectorAll('#playground')
+  console.log('Original playground:', playground)
+  const codeMirrorClass = playground[0].querySelector('.CodeMirror')
+  console.log('Original codeMirrorClass:', codeMirrorClass)
+  codeMirrorClass.value = ''
+  CodeMirror.fromTextArea(codeMirrorClass, {
+    lineNumbers: true,
+    mode: 'javascript',
+    gutters: ['CodeMirror-lint-markers'],
+    lint: true,
+  })
+  console.log(CodeMirror.lint.javascript(code))
   // Locate the existing CodeMirror instance
   // const existingCodeMirrorElement = document.querySelector('.CodeMirror.cm-s-default.CodeMirror-wrap')
 }
 
-// Function to update the CodeMirror value
-// const updateValue = (newValue) => {
-//   if (codeMirrorRef.current) {
-//     codeMirrorRef.current.setValue('TEXT')
-//   }
-// }
-
-// return (
-//   <div>
-//     <h1>Edit Existing CodeMirror Instance</h1>
-//     <p>Current Value:</p>
-//     <textarea value={'value'} onChange={(e) => updateValue(e.target.value)} rows={10} cols={50} />
-//   </div>
-// )
-
-// function getCodeMirrorValue() {
-//   const instances = manipulateCodeMirror()
-//   console.log('instances', instances)
-//   if (instances.length > 0) {
-//     console.log('instances[index].getValue()', instances.getValue())
-//     return { value: instances.getValue() }
-//   } else {
-//     return { error: 'Instance not found' }
-//   }
-// }
-// Function to handle the button click
-// const handleButtonClick = () => {
-//   const mainApp = document.querySelector('main-app')
-//   const shadowRoot = mainApp?.shadowRoot
-//   const screenSet = shadowRoot?.querySelector('app-root')
-//   const iframe = screenSet?.querySelector('iframe.console-app-iframe')
-
-//   const iframeDocument = iframe?.contentDocument || iframe?.contentWindow.document
-//   const mainAppIframe = iframeDocument?.querySelectorAll('#screenSet')
-//   console.log('Original mainAppIframe:', mainAppIframe)
-//   const playground = mainAppIframe?.[0]?.querySelectorAll('#playground')
-//   console.log('Original playground:', playground)
-//   const codeMirrorClass = playground?.[0]?.querySelector('.CodeMirror')
-
-//   if (codeMirrorClass) {
-//     console.log('Original codeMirrorScroll:', playground[0])
-//     manipulateCodeMirror(playground[0])
-//   } else {
-//     console.error('Could not find the CodeMirror div.')
-//   }
-// }
 const ESLINT = () => {
   const useStyles = createUseStyles(styles, { name: 'EmailTemplates' })
   const classes = useStyles()
