34613fa59b4cc71e97e911fc86e51ee025eddb21
Lopes <g.lopes@sap.com>
2024-09-16 17:15:58 +0100

    Adding tracker usage

--- src/routes/prettify-code/useCommonState.js
+++ src/routes/prettify-code/useCommonState.js
@@ -3,7 +3,8 @@ import { useState } from 'react'
 import { selectCredentials } from '../../redux/credentials/credentialsSlice.js'
 import { getApiKey } from '../../redux/utils.js'
 import { selectCurrentSiteInformation } from '../../redux/copyConfigurationExtended/copyConfigurationExtendedSlice.js'
-
+import { trackUsage } from '../../lib/tracker.js'
+const PAGE_TITLE = 'UI Builder'
 export const useCommonState = () => {
   const [showSuccess, setShowSuccess] = useState(false)
   const [showError, setShowError] = useState(false)
@@ -46,7 +47,8 @@ export const onAfterCloseInformationDialogHandle = ({ setShowInfo }) => {
   setShowInfo(false)
 }
 
-export const onAfterCloseSuccessDialogHandle = ({ setShowSuccess }) => {
+export const onAfterCloseSuccessDialogHandle = async ({ setShowSuccess }) => {
+  await trackUsage({ featureName: PAGE_TITLE })
   setShowSuccess(false)
   window.location.reload()
 }
