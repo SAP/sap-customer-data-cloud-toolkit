f440c3906e06b24e3a22a2b0eff51ef0e498f163
Lopes <g.lopes@sap.com>
2024-10-28 15:20:32 +0000

    Adding Button to the UI for the workbench

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import {
   ROUTE_SMS_TEMPLATES,
   ROUTE_PRETTIER,
   ROUTE_PRETTIFY_ALL_SCREENS,
+  ROUTE_DATAFLOW_WORKBENCH,
 } from './inject/constants'
 import CopyConfigurationExtended from './routes/copy-configuration-extended/copy-configuration-extended.component'
 import EmailTemplates from './routes/email-templates/email-templates.component'
@@ -26,6 +27,7 @@ import '@sap_oss/automated-usage-tracking-tool/theme/sap_horizon.css'
 import { requestConsentConfirmation } from './lib/tracker'
 import PrettifyAllScreens from './routes/prettify-code/prettify-all-screens-javascript.component'
 import PrettifySingleScreenComponent from './routes/prettify-code/prettify-single-screen.component'
+import WorkbenchComponent from './routes/work-bench/workbench.component'
 function App() {
   useThemeChange(() => setTheme(getCurrentConsoleTheme()))
   requestConsentConfirmation()
@@ -50,6 +52,9 @@ function App() {
         </div>
         <div className={ROUTE_CONTAINER_CLASS} route={ROUTE_PRETTIFY_ALL_SCREENS}>
           <PrettifyAllScreens />
+        </div>
+        <div className={ROUTE_CONTAINER_CLASS} route={ROUTE_DATAFLOW_WORKBENCH}>
+          <WorkbenchComponent />
         </div>
       </div>
     </ThemeProvider>
---
+++ src/routes/work-bench/workbench.component.jsx
@@ -0,0 +1,4 @@
+import { withTranslation } from 'react-i18next'
+
+const WorkBenchComponent = ({ t }) => {}
+export default withTranslation()(WorkBenchComponent)
