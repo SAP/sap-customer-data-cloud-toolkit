95c803d3b91bfd01e09c14a982285772224d8d27
Lopes <g.lopes@sap.com>
2024-10-30 14:24:33 +0000

    CDCTOOLBOX-453:Removing empty arrow function

--- src/routes/import-accounts/importAccounts.component.jsx
+++ src/routes/import-accounts/importAccounts.component.jsx
@@ -1,1 +1,7 @@
+import { withTranslation } from 'react-i18next'
 
+const ImportAccountsComponent = ({ t }) => {
+  const PAGE_TITLE = 'Import Accounts'
+  console.log(PAGE_TITLE)
+}
+export default withTranslation()(ImportAccountsComponent)
