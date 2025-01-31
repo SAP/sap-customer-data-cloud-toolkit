26502f906a073140223b6ef6faa48ce681ea8d4a
Tiago Gaspar <tiago.gaspar@sap.com>
2025-01-31 17:02:38 +0000

    feature/CDCTOOLBOX-441: Ui changes

--- src/routes/version-control/version-control.component.jsx
+++ src/routes/version-control/version-control.component.jsx
@@ -4,7 +4,7 @@
  */
 import React, { useState, useEffect } from 'react'
 import { withTranslation } from 'react-i18next'
-import { Bar, Input, Button, Dialog, TextArea, Table, TableGrowingMode, TableColumn, TableRow, TableCell } from '@ui5/webcomponents-react'
+import { Bar, Input, Button, Dialog, TextArea, Table, TableGrowingMode, TableColumn, TableRow, TableCell, Title, TitleLevel, FlexBox, Text } from '@ui5/webcomponents-react'
 import { createUseStyles } from 'react-jss'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchCommits, setGitToken, setOwner, setCredentials, selectCommits, selectIsFetching, selectGitToken, selectOwner } from '../../redux/versionControl/versionControlSlice'
@@ -17,7 +17,7 @@ import Cookies from 'js-cookie'
 import { decryptData } from '../../redux/encryptionUtils'
 import styles from './version-control.styles'
 
-const useStyles = createUseStyles(styles, { name: 'Prettier' })
+const useStyles = createUseStyles(styles, { name: 'VersionControl' })
 
 const VersionControlComponent = ({ t }) => {
   const classes = useStyles()
@@ -82,7 +82,7 @@ const VersionControlComponent = ({ t }) => {
     } catch (error) {
       console.error('Error creating backup:', error)
     } finally {
-      setIsDialogOpen(false) // Close the dialog
+      setIsDialogOpen(false)
     }
   }
 
@@ -120,126 +120,175 @@ const VersionControlComponent = ({ t }) => {
 
   return (
     <>
-      <Bar
-        startContent={
-          <>
-            <h2 id="versionControlTitle">{t('VERSION_CONTROL.TITLE')}</h2>
-          </>
-        }
-      ></Bar>
+      <div className={classes.fullContainer}>
+        <Bar
+          design="Header"
+          startContent={
+            <>
+              <Title id="versionControlTitle" data-cy="versionControlTitle" level={TitleLevel.H3} className={classes.titleStyle}>
+                <span className={classes.pageTitleSpanStyle}>{t('VERSION_CONTROL.TITLE')}</span>
+              </Title>
+            </>
+          }
+        />
+        <div className={classes.outerDivStyle}>
+          <div className={classes.headerOuterDivStyle}>
+            <FlexBox className={classes.headerTextFlexboxStyle}>
+              <Text id="importAccountsHeaderText" data-cy="importAccountsHeaderText" className={classes.componentTextStyle}>
+                {t('VERSION_CONTROL.DESCRIPTION.TEXT')}
+              </Text>
+            </FlexBox>
 
-      <Bar
-        className={classes.innerBarStyle}
-        startContent={
-          <>
+            <Bar
+              className={classes.credentialsBlock}
+              startContent={
+                <>
+                  <div>
+                    <h3>{t('VERSION_CONTROL.CREDENTIALS')}</h3>
+                  </div>
+                </>
+              }
+            />
+
+            <Bar
+              className={classes.innerBarStyle}
+              startContent={
+                <>
+                  <div className={classes.credentialsBlock}>
+                    <div className={classes.inputContainer}>
+                      <div className={classes.inputRow}>
+                        <div className={classes.inputField}>
+                          <label className={classes.inputLabel}>{t('VERSION_CONTROL.OWNER')}*</label>
+                          <Input
+                            id="ownerInput"
+                            data-cy="ownerInput"
+                            value={owner}
+                            onChange={handleOwnerChange}
+                            placeholder="Owner"
+                            valueState="Information"
+                            valueStateMessage={<div>{t('VERSION_CONTROL.OWNER_MESSAGE')}</div>}
+                          />
+                        </div>
+                        <div className={classes.inputField}>
+                          <label className={classes.inputLabel}>{t('VERSION_CONTROL.GIT_TOKEN')}*</label>
+                          <Input
+                            id="gitTokenInput"
+                            data-cy="gitTokenInput"
+                            value={gitToken}
+                            onChange={handleGitTokenChange}
+                            placeholder="Git Token"
+                            type="Password"
+                            valueState="Information"
+                            valueStateMessage={<div>{t('VERSION_CONTROL.GIT_TOKEN_MESSAGE')}</div>}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              }
+            />
+
+            <Bar
+              className={classes.backupButtonBar}
+              startContent={
+                <>
+                  <h3 className="h3Title" id="versionList">
+                    {' '}
+                    Version List{' '}
+                  </h3>
+                </>
+              }
+              endContent={
+                <>
+                  <Button
+                    id="backupButton"
+                    data-cy="backupButton"
+                    className={`${classes.singlePrettifyButton} ${classes.backupButton}`}
+                    onClick={onCreateBackupClick}
+                    disabled={!gitToken || !owner}
+                  >
+                    {t('VERSION_CONTROL.BACKUP')}
+                  </Button>
+                </>
+              }
+            />
+
+            <Dialog
+              data-cy="backupDialog"
+              open={isDialogOpen}
+              headerText={t('VERSION_CONTROL.COMMIT_MESSAGE')}
+              footer={
+                <>
+                  <Button data-cy="confirmBackupButton" onClick={onConfirmBackupClick} disabled={filesToUpdate.includes('N/A')}>
+                    {t('VERSION_CONTROL.CONFIRM')}
+                  </Button>
+                  <Button data-cy="cancelBackupButton" onClick={onCancelBackupClick}>
+                    {t('VERSION_CONTROL.CANCEL')}
+                  </Button>
+                </>
+              }
+            >
+              <p>{t('VERSION_CONTROL.UPLOAD_MESSAGE')}</p>
+              <ul>
+                {filesToUpdate.map((file, index) => (
+                  <li key={index}>{file}</li>
+                ))}
+              </ul>
+              <TextArea
+                data-cy="commitMessageInput"
+                value={commitMessage}
+                onInput={handleCommitMessageChange}
+                placeholder={t('VERSION_CONTROL.COMMIT_MESSAGE_PLACEHOLDER')}
+                rows={4}
+                disabled={filesToUpdate.includes('N/A')}
+              />
+            </Dialog>
+
             <div>
-              <h3>{t('VERSION_CONTROL.CREDENTIALS')}</h3>
-              <div className={classes.inputContainer}>
-                <div className={classes.inputRow}>
-                  <div className={classes.inputField}>
-                    <label className={classes.inputLabel}>{t('VERSION_CONTROL.OWNER')}</label>
-                    <Input
-                      id="ownerInput"
-                      data-cy="ownerInput"
-                      value={owner}
-                      onChange={handleOwnerChange}
-                      placeholder="Owner"
-                      valueState="Information"
-                      valueStateMessage={<div>{t('VERSION_CONTROL.OWNER_MESSAGE')}</div>}
-                    />
-                  </div>
-                  <div className={classes.inputField}>
-                    <label className={classes.inputLabel}>{t('VERSION_CONTROL.GIT_TOKEN')}</label>
-                    <Input
-                      id="gitTokenInput"
-                      data-cy="gitTokenInput"
-                      value={gitToken}
-                      onChange={handleGitTokenChange}
-                      placeholder="Git Token"
-                      type="Password"
-                      valueState="Information"
-                      valueStateMessage={<div>{t('VERSION_CONTROL.GIT_TOKEN_MESSAGE')}</div>}
-                    />
-                  </div>
-                </div>
-                <Button id="backupButton" data-cy="backupButton" className={classes.singlePrettifyButton} onClick={onCreateBackupClick} disabled={!gitToken || !owner}>
-                  {t('VERSION_CONTROL.BACKUP')}
-                </Button>
+              <div className={classes.tableContainer}>
+                <Table
+                  stickyColumnHeader={true}
+                  growing={TableGrowingMode.Scroll}
+                  onLoadMore={onLoadMore}
+                  columns={
+                    <>
+                      <TableColumn style={{ minWidth: '70px', width: '70px' }}>{t('VERSION_CONTROL.DATE')}</TableColumn>
+                      <TableColumn style={{ minWidth: '85px', width: '85px' }}>{t('VERSION_CONTROL.ID')}</TableColumn>
+                      <TableColumn>{t('VERSION_CONTROL.COMMIT_MESSAGE')}</TableColumn>
+                      <TableColumn className={classes.restoreRowTitle} style={{ width: '45px' }}>
+                        {t('VERSION_CONTROL.ACTION')}
+                      </TableColumn>
+                    </>
+                  }
+                >
+                  {commits.length === 0 ? (
+                    <TableRow>
+                      <TableCell>{t('VERSION_CONTROL.NO_COMMITS')}</TableCell>
+                    </TableRow>
+                  ) : (
+                    commits.map((commit, index) => (
+                      <TableRow key={index}>
+                        <TableCell className={classes.dateCollumnStyle}>{new Date(commit.commit.committer.date).toLocaleString()}</TableCell>
+                        <TableCell className={classes.idCollumnStyle}>{commit.sha.substring(0, 7)}</TableCell>
+                        <TableCell className={classes.messageCollumnStyle}>{commit.commit.message}</TableCell>
+                        <TableCell className={classes.restoreButton}>
+                          <Button
+                            id={`commitRevertButton-${index}`}
+                            data-cy="revertCommitButton"
+                            className={classes.singlePrettifyRestoreButton}
+                            onClick={() => onCommitRevertClick(commit.sha)}
+                          >
+                            {t('VERSION_CONTROL.RESTORE')}
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  )}
+                </Table>
               </div>
             </div>
-          </>
-        }
-      ></Bar>
-
-      <Dialog
-        data-cy="backupDialog"
-        open={isDialogOpen}
-        headerText={t('VERSION_CONTROL.COMMIT_MESSAGE')}
-        footer={
-          <>
-            <Button data-cy="confirmBackupButton" onClick={onConfirmBackupClick} disabled={filesToUpdate.includes('N/A')}>
-              {t('VERSION_CONTROL.CONFIRM')}
-            </Button>
-            <Button data-cy="cancelBackupButton" onClick={onCancelBackupClick}>
-              {t('VERSION_CONTROL.CANCEL')}
-            </Button>
-          </>
-        }
-      >
-        <p>{t('VERSION_CONTROL.UPLOAD_MESSAGE')}</p>
-        <ul>
-          {filesToUpdate.map((file, index) => (
-            <li key={index}>{file}</li>
-          ))}
-        </ul>
-        <TextArea
-          data-cy="commitMessageInput"
-          value={commitMessage}
-          onInput={handleCommitMessageChange}
-          placeholder={t('VERSION_CONTROL.COMMIT_MESSAGE_PLACEHOLDER')}
-          rows={4}
-          disabled={filesToUpdate.includes('N/A')}
-        />
-      </Dialog>
-
-      <div className={classes.fullHeightContainer}>
-        <div className={classes.tableContainer}>
-          <Table
-            growing={TableGrowingMode.Scroll}
-            onLoadMore={onLoadMore}
-            columns={
-              <>
-                <TableColumn style={{ width: '150px' }}>{t('VERSION_CONTROL.ID')}</TableColumn>
-                <TableColumn style={{ width: '200px' }}>{t('VERSION_CONTROL.DATE')}</TableColumn>
-                <TableColumn style={{ width: '400px' }}>{t('VERSION_CONTROL.COMMIT_MESSAGE')}</TableColumn>
-                <TableColumn style={{ width: '150px' }}>{t('VERSION_CONTROL.ACTION')}</TableColumn>
-              </>
-            }
-          >
-            {commits.length === 0 ? (
-              <TableRow>
-                <TableCell colSpan="4">{t('VERSION_CONTROL.NO_COMMITS')}</TableCell>
-              </TableRow>
-            ) : (
-              commits.map((commit, index) => (
-                <TableRow key={index}>
-                  <TableCell>{commit.sha.substring(0, 7)}</TableCell>
-                  <TableCell>{new Date(commit.commit.committer.date).toLocaleString()}</TableCell>
-                  <TableCell>{commit.commit.message}</TableCell>
-                  <TableCell>
-                    <Button
-                      id={`commitRevertButton-${index}`}
-                      data-cy="revertCommitButton"
-                      className={classes.singlePrettifyButton}
-                      onClick={() => onCommitRevertClick(commit.sha)}
-                    >
-                      {t('VERSION_CONTROL.RESTORE')}
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))
-            )}
-          </Table>
+          </div>
         </div>
       </div>
     </>
--- src/routes/version-control/version-control.styles.js
+++ src/routes/version-control/version-control.styles.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright: Copyright 2023 SAP SE or an SAP affiliate company and cdc-tools-chrome-extension contributors
+ * License: Apache-2.0
+ */
+import { TextAlign } from '@ui5/webcomponents-react'
+import { spacing } from '@ui5/webcomponents-react-base'
+
 const styles = {
   innerBarStyle: {
     display: 'contents',
@@ -7,16 +14,28 @@ const styles = {
     backgroundColor: '#fff',
     justifyContent: 'flex-start !important',
     marginBottom: '100px',
-    height: '170px',
+    height: '120px',
+    borderRadius: '2px 2px 10px 10px',
   },
   singlePrettifyButton: {
-    marginTop: '10px',
-    alignSelf: 'flex-end',
+    height: '30px',
     backgroundColor: '#0070f3',
     color: '#fff',
-    minWidth: 'fit-content',
+    minWidth: '-webkit-fit-content', // for Safari
+    minWidth: 'fit-content', // Added min-width property
     '&:hover': {
       backgroundColor: '#005bb5',
+    },
+  },
+  singlePrettifyRestoreButton: {
+    height: '23px',
+    backgroundColor: '#fff',
+    color: '#0070f3',
+    minWidth: '-webkit-fit-content', // for Safari
+    minWidth: 'fit-content', // Added min-width property
+    '&:hover': {
+      backgroundColor: '#005bb5',
+      color: '#fff',
     },
   },
   tableContainer: {
@@ -24,7 +43,6 @@ const styles = {
     overflowY: 'auto',
     display: 'flex',
     flexDirection: 'column',
-    maxHeight: 'calc(100vh - 280px)',
   },
   commitTable: {
     width: '100%',
@@ -69,6 +87,70 @@ const styles = {
     flexDirection: 'column',
     height: '100vh',
     overflow: 'hidden',
+  },
+  descriptionText: {
+    height: '40px',
+    alignContent: 'center',
+  },
+  backupButtonBar: {
+    alignContent: 'center',
+    marginTop: '10px',
+    borderRadius: '10px 10px 2px 2px',
+    height: '70px',
+  },
+  restoreRowTitle: {
+    marginRight: '3px',
+    textAlign: 'rigth !important',
+  },
+  restoreButton: {
+    textAlign: 'right',
+    paddingRight: '40px',
+  },
+  backupButton: {
+    marginRight: '30px',
+  },
+  credentialsBlock: {
+    borderRadius: '10px 10px 2px 2px',
+  },
+  fullContainer: {
+    overflowY: 'auto',
+    width: '100%',
+  },
+  titleSpanStyle: { fontSize: '24px' },
+  pageTitleSpanStyle: {
+    ...spacing.sapUiTinyMarginBegin,
+  },
+  headerOuterDivStyle: {
+    width: '100%',
+  },
+  outerDivStyle: {
+    composes: 'cdc-tools-background',
+    maxHeight: '80vh',
+    ...spacing.sapUiSmallMargin,
+  },
+  headerTextFlexboxStyle: {
+    ...spacing.sapUiTinyMarginBegin,
+    ...spacing.sapUiTinyMarginTop,
+    ...spacing.sapUiSmallMarginBottom,
+    marginLeft: '15px',
+  },
+  idCollumnStyle: {
+    minWidth: '55px',
+    width: 'fit-content',
+  },
+  dateCollumnStyle: {
+    minWidth: '134px',
+    width: 'fit-content',
+  },
+  messageCollumnStyle: {
+    width: 'fit-content',
+
+    display: 'block' /* or inline-block */,
+    textOverflow: 'ellipsis',
+    wordWrap: 'break-word',
+    overflow: 'hidden',
+    maxHeight: '3.6em',
+    lineHeight: '1.8em',
   },
 }
 
