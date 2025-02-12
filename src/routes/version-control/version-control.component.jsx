a09e986c57d20b7794656848bfc3a5fbb161fcf0
Tiago Gaspar <tiago.gaspar@sap.com>
2025-02-12 14:33:06 +0000

    feature/CDCTOOLBOX-441-UI: UI changes

--- src/routes/version-control/version-control.component.jsx
+++ src/routes/version-control/version-control.component.jsx
@@ -22,8 +22,9 @@ import {
   Text,
   ValueState,
   Card,
-  CardHeader,
+
   Grid,
+  Label,
 } from '@ui5/webcomponents-react'
 import { createUseStyles } from 'react-jss'
 import { useSelector, useDispatch } from 'react-redux'
@@ -161,7 +162,7 @@ const VersionControlComponent = ({ t }) => {
 
   const renderStatusMessage = () => {
     if (!gitToken || !owner) {
-      return <Bar className={classes.noCommitsBar} startContent={<Text>{t('VERSION_CONTROL.INSERT_CREDENTIALS')}</Text>} />
+      return <div></div>
     } else if (commits.length === 0) {
       return <Bar className={classes.noCommitsBar} startContent={<Text>{t('VERSION_CONTROL.NO_COMMITS')}</Text>} />
     }
@@ -203,7 +204,10 @@ const VersionControlComponent = ({ t }) => {
               </Text>
             </FlexBox>
 
-            <Card header={<CardHeader titleText={t('VERSION_CONTROL.CREDENTIALS')} />}>
+            <Card className={classes.credentialsCard}>
+              <Title className={classes.cardTitle} level="H4">
+                {t('VERSION_CONTROL.CREDENTIALS')}
+              </Title>
               <Grid>
                 <>
                   <div style={{ display: 'inline-flex' }} className={classes.currentInfoContainer} data-layout-span="XL5 L5 M5 S5">
@@ -219,6 +223,7 @@ const VersionControlComponent = ({ t }) => {
                         placeholder="Owner"
                         valueState="Information"
                         valueStateMessage={<div>{t('VERSION_CONTROL.OWNER_MESSAGE')}</div>}
+                        required
                       />
                     </div>
 
@@ -235,83 +240,35 @@ const VersionControlComponent = ({ t }) => {
                         type="Password"
                         valueState="Information"
                         valueStateMessage={<div>{t('VERSION_CONTROL.GIT_TOKEN_MESSAGE')}</div>}
+                        required
                       />
                     </div>
+                    {(!gitToken || !owner) && <div className={classes.warningMessage}>{t('VERSION_CONTROL.INSERT_CREDENTIALS')}</div>}
                   </div>
                 </>
               </Grid>
             </Card>
-            {/* <Bar
-              className={classes.credentialsBlock}
-              startContent={
-                <>
-                  <div>
-                    <h3>{t('VERSION_CONTROL.CREDENTIALS')}</h3>
-                  </div>
-                </>
-              }
-            /> */}
-
-            {/* <Bar
-              className={classes.innerBarStyle}
-              startContent={
-                <>
-                  <div className={classes.credentialsBlock}>
-                    <div className={classes.inputContainer}>
-                      <div className={classes.inputRow}>
-                        <div className={classes.inputField}>
-                          <label className={classes.inputLabel}>{t('VERSION_CONTROL.OWNER')}*</label>
-                          <Input
-                            id="ownerInput"
-                            data-cy="ownerInput"
-                            value={owner}
-                            onChange={handleOwnerChange}
-                            placeholder="Owner"
-                            valueState="Information"
-                            valueStateMessage={<div>{t('VERSION_CONTROL.OWNER_MESSAGE')}</div>}
-                          />
-                        </div>
-                        <div className={classes.inputField}>
-                          <label className={classes.inputLabel}>{t('VERSION_CONTROL.GIT_TOKEN')}*</label>
-                          <Input
-                            id="gitTokenInput"
-                            data-cy="gitTokenInput"
-                            value={gitToken}
-                            onChange={handleGitTokenChange}
-                            placeholder="Git Token"
-                            type="Password"
-                            valueState="Information"
-                            valueStateMessage={<div>{t('VERSION_CONTROL.GIT_TOKEN_MESSAGE')}</div>}
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </>
-              }
-            /> */}
 
             <Card className={classes.versionListTable}>
               <>
-                <div className={classes.tableTitle}>
-                  <span id="versionList">
-                    <Text className={classes.versionListText}>{t('VERSION_CONTROL.VERSION_LIST')}</Text>
-                  </span>
-                  <span className={classes.flexButton}>
-                    <Button
-                      id="backupButton"
-                      data-cy="backupButton"
-                      design="Emphasized"
-                      className={`${classes.singlePrettifyButton} ${classes.backupButton}`}
-                      onClick={onCreateBackupClick}
-                      disabled={!gitToken || !owner}
-                    >
-                      {t('VERSION_CONTROL.BACKUP')}
-                    </Button>
-                  </span>
+                <div id="versionList" className={classes.cardTitle}>
+                  <Title level="H4" className={classes.versionListText}>
+                    {t('VERSION_CONTROL.VERSION_LIST')}
+                  </Title>
+
+                  <Button
+                    id="backupButton"
+                    data-cy="backupButton"
+                    design="Emphasized"
+                    className={`${classes.singlePrettifyButton} ${classes.backupButton} ${classes.flexButton}`}
+                    onClick={onCreateBackupClick}
+                    disabled={!gitToken || !owner}
+                  >
+                    {t('VERSION_CONTROL.BACKUP')}
+                  </Button>
                 </div>
               </>
-              <Dialog
+              {/* <Dialog
                 open={isDialogOpen}
                 className="ui-dialog"
                 headerText={t('VERSION_CONTROL.COMMIT_MESSAGE')}
@@ -351,7 +308,7 @@ const VersionControlComponent = ({ t }) => {
                     </Button>
                   </div>
                 }
-              />
+              /> */}
 
               <div>
                 {renderStatusMessage() ?? (
@@ -374,14 +331,18 @@ const VersionControlComponent = ({ t }) => {
                       {commits.map((commit, index) => (
                         <TableRow key={index}>
                           <TableCell className={classes.dateCollumnStyle}>{new Date(commit.commit.committer.date).toLocaleString()}</TableCell>
-                          <TableCell className={classes.idCollumnStyle}>{commit.sha.substring(0, 7)}</TableCell>
+                          <TableCell className={classes.idCollumnStyle}>
+                            <a href={commit.html_url} target="_blank" rel="noopener noreferrer">
+                              {commit.sha.substring(0, 7)}
+                            </a>
+                          </TableCell>
                           <TableCell className={classes.messageCollumnStyle}>{commit.commit.message}</TableCell>
                           <TableCell className={classes.restoreButton}>
                             <Button
                               design="Default"
                               id={`commitRevertButton-${index}`}
                               data-cy="revertCommitButton"
-                              // className={classes.singlePrettifyRestoreButton}
+                              className={classes.singlePrettifyRestoreButton}
                               onClick={() => onCommitRevertClick(commit.sha)}
                             >
                               {t('VERSION_CONTROL.RESTORE')}
@@ -397,21 +358,24 @@ const VersionControlComponent = ({ t }) => {
             <Dialog
               open={isDialogOpen}
               className="ui-dialog"
-              headerText={t('VERSION_CONTROL.COMMIT_MESSAGE')}
-              state={ValueState.Information}
               onAfterClose={onCancelBackupClick}
               id="backupDialog"
               data-cy="backupDialog"
               header={
-                <div id="header" className={classes.headerOuterDivStyle2}>
-                  {t('VERSION_CONTROL.UPLOAD_MESSAGE')}
+                <div id="header" className={classes.headerOuterDivStyle3}>
+                  <div className={classes.headerInnerDivStyle}>{t('VERSION_CONTROL.BACKUP.INFORMATION')}</div>
                 </div>
               }
               children={
                 <div className={classes.specifyFileLableStyle}>
+                  <div className={classes.specifyFileLableStyle}>
+                    <Label id="specifyFileLabel">{t('VERSION_CONTROL.UPLOAD_MESSAGE')}</Label>
+                  </div>
                   <ul>
                     {filesToUpdate.map((file, index) => (
-                      <li key={index}>{file}</li>
+                      <li className={classes.filesToUpdate} key={index}>
+                        <Text>{file}</Text>
+                      </li>
                     ))}
                   </ul>
                   <TextArea
