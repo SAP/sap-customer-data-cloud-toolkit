78e758ea7df590cc2c11aa0bd91bf252e272d3d9
Lopes <g.lopes@sap.com>
2025-03-17 15:25:03 +0000

    feature/CDCTOOLBOX-462:Adding profile.email to the lite account type

--- src/services/importAccounts/schemaImport/schemaImport.test.js
+++ src/services/importAccounts/schemaImport/schemaImport.test.js
@@ -21,54 +21,61 @@ describe('Import Account - SchemaImport test suite', () => {
   const targetDataCenter = 'eu1'
   const targetApiKey = 'targetApiKey'
   const schemaImport = new SchemaImportFields(credentials, targetApiKey, targetDataCenter)
+
   beforeEach(() => {
     jest.restoreAllMocks()
+    jest.clearAllMocks()
   })
+
+  test('clean schema data successfully', () => {
+    const schemaResponse = expectedSchemaResponse
+    schemaImport.cleanSchemaData(schemaResponse)
+    expect(schemaResponse).toEqual(expectedSchemaResponseCleaned)
+  })
+
   test('remove field from addresses schema successfully', () => {
     const schemaResponse = { ...expectedSchemaResponse }
     delete schemaResponse.statusCode
     delete schemaResponse.statusReason
     delete schemaResponse.time
     delete schemaResponse.callId
+    delete schemaResponse.errorCode
     schemaImport.removeFieldFromAddressesSchema(schemaResponse)
+
     expect(schemaResponse).toEqual(expectedSchemaCleanAddress)
   })
-  test('clean schema data successfully', () => {
-    const schemaResponse = expectedSchemaResponse
-    schemaImport.cleanSchemaData(schemaResponse)
-    expect(schemaResponse).toEqual(expectedSchemaResponseCleaned)
+
+  test('export transformed schema data successfully', async () => {
+    expectedSchemaResponse.errorCode = 0
+    axios.mockResolvedValueOnce({ data: expectedSchemaResponse })
+    const response = await schemaImport.exportTransformedSchemaData()
+    expect(response).toEqual(transformedSchema)
   })
   test('export schema data successfully', async () => {
     axios.mockResolvedValueOnce({ data: expectedSchemaResponse })
     const response = await schemaImport.exportSchemaData()
     expect(response).toEqual(expectedSchemaResponseCleaned)
   })
-  test('export transformed schema data successfully', async () => {
-    axios.mockResolvedValueOnce({ data: expectedSchemaResponse })
-    const response = await schemaImport.exportTransformedSchemaData()
-    expect(response).toEqual(transformedSchema)
-  })
+
   test('export lite schema data successfully', async () => {
+    expectedSchemaResponse.errorCode = 0
     axios.mockResolvedValueOnce({ data: expectedSchemaResponse })
     const response = await schemaImport.exportLiteSchemaData()
     expect(response).toEqual(expectedLiteSchemaResponse)
   })
+
   test('get schema data successfully', async () => {
     axios.mockResolvedValueOnce({ data: expectedSchemaResponse })
     const response = await schemaImport.getSchema()
     expect(response).toEqual(expectedSchemaResponse)
   })
+
   test('clean lite schema data successfully', () => {
     const schemaResponse = { ...expectedSchemaResponse }
     const cleanedSchema = schemaImport.cleanLiteSchemaData(schemaResponse)
     expect(cleanedSchema).toEqual(expectedTransformedLiteResponse)
   })
 
-  test('remove field from subscription schema successfully', () => {
-    const schemaResponse = { ...expectedSchemaResponse }
-    schemaImport.removeFieldFromSubscriptionSchema(schemaResponse)
-    expect(schemaResponse).toEqual(expectedSchemaResponseWithoutFields)
-  })
   test('handle error in export schema data', async () => {
     const errorResponse = { errorCode: 1, errorMessage: 'Error' }
     axios.mockResolvedValueOnce({ data: errorResponse })
