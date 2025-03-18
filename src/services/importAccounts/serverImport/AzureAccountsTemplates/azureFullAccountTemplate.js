c1bf9a8110876086b48c6968eb8a7c088056e75c
Lopes <g.lopes@sap.com>
2025-03-18 12:25:44 +0000

    feature/CDCTOOLBOX-462:Numbering the dataflows steps

--- src/services/importAccounts/serverImport/AzureAccountsTemplates/azureFullAccountTemplate.js
+++ src/services/importAccounts/serverImport/AzureAccountsTemplates/azureFullAccountTemplate.js
@@ -5,7 +5,7 @@ export const azureFullAccountTemplate = `
  "description": "Import Full Account - Toolkit",
  "steps": [
   {
-   "id": "3. Transform to CDC Structure",
+   "id": "03. Transform to CDC Structure",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQoNCiAgICAvLyBjb252ZXJ0cyBpbnB1dDogInByb2ZpbGUuZmlyc3ROYW1lIjoiVGVzdCAxIiwicHJvZmlsZS5sYXN0TmFtZSI6IkVCIiwicHJvZmlsZS5lZHVjYXRpb24uMC5kZWdyZWUiOiJHcmFkdWF0aW9uIiwicHJvZmlsZS5lZHVjYXRpb24uMC5maWVsZE9mU3R1ZHkiOiJTY2llbmNlIg0KICAgIC8vIHRvIG91dHB1dDogeyJwcm9maWxlIjp7ImVtYWlsIjoidGVzdDg4MkBtYWlsLmNvbSIsImZpcnN0TmFtZSI6IlRlc3QgMSIsImxhc3ROYW1lIjoiRUIiLCJlZHVjYXRpb24iOlt7ImRlZ3JlZSI6IkdyYWR1YXRpb24iLCJmaWVsZE9mU3R1ZHkiOiJTY2llbmNlIn1dfQ0KDQogICAgbG9nZ2VyLmluZm8oIklucHV0IFBheWxvYWQiLCByZWNvcmQpOw0KDQogICAgbGV0IHJlc3VsdCA9IHt9Ow0KDQogICAgZm9yIChsZXQga2V5IGluIHJlY29yZCkgew0KICAgICAgICBpZiAocmVjb3JkLmhhc093blByb3BlcnR5KGtleSkpIHsNCiAgICAgICAgICAgIGNvbnN0IHZhbHVlID0gcmVjb3JkW2tleV07DQogICAgICAgICAgICBjb25zdCBrZXlzQXJyYXkgPSBrZXkuc3BsaXQoJy4nKTsNCg0KICAgICAgICAgICAgY29uc3QgbmVzdGVkT2JqZWN0ID0gYnVpbGROZXN0ZWRPYmplY3Qoa2V5c0FycmF5LCB2YWx1ZSk7DQogICAgICAgICAgICByZXN1bHQgPSBtZXJnZURlZXAocmVzdWx0LCBuZXN0ZWRPYmplY3QpOw0KICAgICAgICB9DQogICAgfQ0KDQogICAgbG9nZ2VyLmluZm8oIlRyYW5zZm9ybWVkIFBheWxvYWQiLCByZXN1bHQpOw0KICAgIHJldHVybiByZXN1bHQ7DQp9DQoNCmZ1bmN0aW9uIGJ1aWxkTmVzdGVkT2JqZWN0KGtleXMsIHZhbHVlKSB7DQogICAgLy8gUmVjdXJzaXZlIGZ1bmN0aW9uIHRvIGJ1aWxkIGEgbmVzdGVkIG9iamVjdCBvciBhcnJheQ0KICAgIGlmIChrZXlzLmxlbmd0aCA9PT0gMSkgew0KICAgICAgICByZXR1cm4gew0KICAgICAgICAgICAgW2tleXNbMF1dOiB2YWx1ZQ0KICAgICAgICB9Ow0KICAgIH0NCg0KICAgIGNvbnN0IGtleSA9IGtleXNbMF07DQogICAgY29uc3QgbmV4dEtleSA9IGtleXNbMV07DQoNCiAgICAvLyBJZiBuZXh0IGtleSBpcyBhIG51bWJlciwgdHJlYXQgaXQgYXMgYW4gYXJyYXkNCiAgICBpZiAoIWlzTmFOKHBhcnNlSW50KG5leHRLZXkpKSkgew0KICAgICAgICBsZXQgYXJyID0gW107DQogICAgICAgIGFycltwYXJzZUludChuZXh0S2V5KV0gPSBidWlsZE5lc3RlZE9iamVjdChrZXlzLnNsaWNlKDIpLCB2YWx1ZSk7DQogICAgICAgIHJldHVybiB7DQogICAgICAgICAgICBba2V5XTogYXJyDQogICAgICAgIH07DQogICAgfQ0KDQogICAgcmV0dXJuIHsNCiAgICAgICAgW2tleV06IGJ1aWxkTmVzdGVkT2JqZWN0KGtleXMuc2xpY2UoMSksIHZhbHVlKQ0KICAgIH07DQp9DQoNCmZ1bmN0aW9uIG1lcmdlRGVlcCh0YXJnZXQsIHNvdXJjZSkgew0KICAgIC8vIEhlbHBlciBmdW5jdGlvbiB0byBtZXJnZSBuZXN0ZWQgb2JqZWN0cyBvciBhcnJheXMNCiAgICBmb3IgKGxldCBrZXkgaW4gc291cmNlKSB7DQogICAgICAgIGlmIChzb3VyY2Vba2V5XSBpbnN0YW5jZW9mIE9iamVjdCAmJiBrZXkgaW4gdGFyZ2V0KSB7DQogICAgICAgICAgICBpZiAoQXJyYXkuaXNBcnJheSh0YXJnZXRba2V5XSkgJiYgQXJyYXkuaXNBcnJheShzb3VyY2Vba2V5XSkpIHsNCiAgICAgICAgICAgICAgICAvLyBNZXJnZSBhcnJheXMgYnkgaW5kZXgsIG1ha2luZyBzdXJlIHRvIGF2b2lkIG51bGwgdmFsdWVzDQogICAgICAgICAgICAgICAgdGFyZ2V0W2tleV0gPSB0YXJnZXRba2V5XS5tYXAoKGl0ZW0sIGluZGV4KSA9PiB7DQogICAgICAgICAgICAgICAgICAgIHJldHVybiBpdGVtICE9PSB1bmRlZmluZWQgPyBtZXJnZURlZXAoaXRlbSwgc291cmNlW2tleV1baW5kZXhdKSA6IHNvdXJjZVtrZXldW2luZGV4XTsNCiAgICAgICAgICAgICAgICB9KS5jb25jYXQoc291cmNlW2tleV0uc2xpY2UodGFyZ2V0W2tleV0ubGVuZ3RoKSk7DQogICAgICAgICAgICB9IGVsc2Ugew0KICAgICAgICAgICAgICAgIE9iamVjdC5hc3NpZ24oc291cmNlW2tleV0sIG1lcmdlRGVlcCh0YXJnZXRba2V5XSwgc291cmNlW2tleV0pKTsNCiAgICAgICAgICAgIH0NCiAgICAgICAgfSBlbHNlIHsNCiAgICAgICAgICAgIHRhcmdldFtrZXldID0gc291cmNlW2tleV07DQogICAgICAgIH0NCiAgICB9DQogICAgcmV0dXJuIHRhcmdldDsNCn0=",
@@ -13,11 +13,11 @@ export const azureFullAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "4. loginIds"
+    "04. loginIds"
    ]
   },
   {
-   "id": "1. azure.blob",
+   "id": "01. azure.blob",
    "type": "datasource.read.azure.blob",
    "params": {
     "accountName": "{{accountName}}",
@@ -27,11 +27,11 @@ export const azureFullAccountTemplate = `
     "blobPrefix": "{{blobPrefix}}"
    },
    "next": [
-    "2. file.parse.dsv"
+    "02. file.parse.dsv"
    ]
   },
   {
-   "id": "2. file.parse.dsv",
+   "id": "02. file.parse.dsv",
    "type": "file.parse.dsv",
    "params": {
     "columnSeparator": ",",
@@ -40,11 +40,11 @@ export const azureFullAccountTemplate = `
     "addFilename": false
    },
    "next": [
-    "3. Transform to CDC Structure"
+    "03. Transform to CDC Structure"
    ]
   },
   {
-   "id": "5. Subscriptions",
+   "id": "05. Subscriptions",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQogICAgLy8gQ29udmVydHMgaW5wdXQ6ICJzdWJzY3JpcHRpb25zLm5ld3NsZXR0ZXIuY29tbWVyY2lhbC5pc1N1YnNjcmliZWQiOiJUUlVFIg0KICAgIC8vIHRvIG91dHB1dDogeyJzdWJzY3JpcHRpb25zIjogeyJuZXdzbGV0dGVyIjp7ImNvbW1lcmNpYWwiOnsiZW1haWwiOnsiaXNTdWJzY3JpYmVkIjoiVFJVRSJ9fX19fQ0KDQogICAgaWYgKHJlY29yZC5oYXNPd25Qcm9wZXJ0eSgic3Vic2NyaXB0aW9ucyIpKSB7DQogICAgICAgIHJlY29yZC5zdWJzY3JpcHRpb25zID0gYWRkRW1haWxCZWZvcmVJc1N1YnNjcmliZWQocmVjb3JkLnN1YnNjcmlwdGlvbnMpOw0KICAgICAgICBsb2dnZXIuaW5mbygiVHJhbnNmb3JtZWQgU3Vic2NyaXB0aW9ucyIsIHJlY29yZC5zdWJzY3JpcHRpb25zKTsNCiAgICB9DQoNCiAgICByZXR1cm4gcmVjb3JkOw0KfQ0KDQpmdW5jdGlvbiBhZGRFbWFpbEJlZm9yZUlzU3Vic2NyaWJlZChvYmopIHsNCiAgICAvLyBSZWN1cnNpdmUgZnVuY3Rpb24gdG8gdHJhdmVyc2UgYW5kIGZpbmQgdGhlIG9iamVjdCB3aXRoICJpc1N1YnNjcmliZWQiDQogICAgZnVuY3Rpb24gdHJhdmVyc2Uobm9kZSkgew0KICAgICAgICBpZiAodHlwZW9mIG5vZGUgIT09ICdvYmplY3QnIHx8IG5vZGUgPT09IG51bGwpIHJldHVybjsNCg0KICAgICAgICBjb25zdCBrZXlzID0gT2JqZWN0LmtleXMobm9kZSk7DQoNCiAgICAgICAgLy8gQ2hlY2sgaWYgdGhlIGN1cnJlbnQgbm9kZSBoYXMgImlzU3Vic2NyaWJlZCIga2V5DQogICAgICAgIGlmIChrZXlzLmluY2x1ZGVzKCJpc1N1YnNjcmliZWQiKSkgew0KICAgICAgICAgICAgY29uc3QgY29weSA9IHsNCiAgICAgICAgICAgICAgICAuLi5ub2RlDQogICAgICAgICAgICB9Ow0KDQogICAgICAgICAgICAvLyBDbGVhciB0aGUgY3VycmVudCBvYmplY3QgYW5kIHdyYXAgaXQgd2l0aCAiZW1haWwiDQogICAgICAgICAgICBmb3IgKGNvbnN0IGtleSBvZiBrZXlzKSB7DQogICAgICAgICAgICAgICAgZGVsZXRlIG5vZGVba2V5XTsNCiAgICAgICAgICAgIH0NCg0KICAgICAgICAgICAgbm9kZS5lbWFpbCA9IGNvcHk7DQogICAgICAgICAgICByZXR1cm47DQogICAgICAgIH0NCg0KICAgICAgICAvLyBSZWN1cnNpdmVseSB0cmF2ZXJzZSB0aGUgY2hpbGQgbm9kZXMNCiAgICAgICAgZm9yIChjb25zdCBrZXkgb2Yga2V5cykgew0KICAgICAgICAgICAgaWYgKHR5cGVvZiBub2RlW2tleV0gPT09ICdvYmplY3QnICYmIG5vZGVba2V5XSAhPT0gbnVsbCkgew0KICAgICAgICAgICAgICAgIHRyYXZlcnNlKG5vZGVba2V5XSk7DQogICAgICAgICAgICB9DQogICAgICAgIH0NCiAgICB9DQoNCiAgICAvLyBTdGFydCB0cmF2ZXJzYWwgZnJvbSB0aGUgcm9vdCBvYmplY3QNCiAgICB0cmF2ZXJzZShvYmopOw0KDQogICAgcmV0dXJuIG9iajsNCn0=",
@@ -52,12 +52,12 @@ export const azureFullAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "6.1 If UID is Present",
-    "6. If UID Is Not Present"
+    "06.1 If UID is Present",
+    "06. If UID Is Not Present"
    ]
   },
   {
-   "id": "6.2 Account lookup using UID",
+   "id": "06.2 Account lookup using UID",
    "type": "datasource.lookup.gigya.account",
    "params": {
     "select": "UID",
@@ -79,14 +79,14 @@ export const azureFullAccountTemplate = `
     "separateMultiMatches": false
    },
    "next": [
-    "7. Handle UID"
+    "07. Handle UID"
    ],
    "error": [
-    "6.3 Lookup Failed"
+    "06.3 Lookup Failed"
    ]
   },
   {
-   "id": "7. Handle UID",
+   "id": "07. Handle UID",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQoNCiAgICBsZXQgVUlEOw0KICAgIGxldCByZXF1ZXN0ID0ge307DQoNCiAgICBpZiAocmVjb3JkLlVJRCkgew0KICAgICAgICBVSUQgPSByZWNvcmQuVUlEOw0KICAgIH0gZWxzZSB7DQogICAgICAgIFVJRCA9ICd4eHh4eHh4eHh4eHg0eHh4eXh4eHh4eHh4eHh4eHh4eCcucmVwbGFjZSgvW3h5XS9nLCBmdW5jdGlvbihjKSB7DQogICAgICAgICAgICB2YXIgciA9IE1hdGgucmFuZG9tKCkgKiAxNiB8IDAsDQogICAgICAgICAgICAgICAgdiA9IGMgPT0gJ3gnID8gciA6IChyICYgMHgzIHwgMHg4KTsNCiAgICAgICAgICAgIHJldHVybiB2LnRvU3RyaW5nKDE2KTsNCiAgICAgICAgfSk7DQogICAgfQ0KICAgIHJlY29yZC51aWQgPSBVSUQ7DQogICAgZGVsZXRlIHJlY29yZC5VSUQ7DQoNCiAgICByZXR1cm4gcmVjb3JkOw0KfQ==",
@@ -94,7 +94,7 @@ export const azureFullAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "8. Handle unsupported schema"
+    "08. Handle unsupported schema"
    ]
   },
   {
@@ -206,7 +206,7 @@ export const azureFullAccountTemplate = `
    ]
   },
   {
-   "id": "9. Import Account Request Logger",
+   "id": "09. Import Account Request Logger",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQoNCiAgbG9nZ2VyLmluZm8oIkltcG9ydCBBY2NvdW50IFJlcXVlc3QiLCByZWNvcmQpOw0KICAgIHJldHVybiByZWNvcmQ7DQp9",
@@ -242,7 +242,7 @@ export const azureFullAccountTemplate = `
    ]
   },
   {
-   "id": "4. loginIds",
+   "id": "04. loginIds",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQoNCiAgICAvLyBjb252ZXJ0cyBpbnB1dDogImxvZ2luSWRzLnVzZXJuYW1lIjoiVVMxMDEiLCJsb2dpbklkcy5lbWFpbHMiOiJ0ZXN0MUBtYWlsLmNvbSIsImxvZ2luSWRzLnVudmVyaWZpZWRFbWFpbHMiOiIiDQogICAgLy8gdG8gb3V0cHV0OiB7InVzZXJuYW1lIjoiVVMxMDEiLCJlbWFpbHMiOlsidGVzdDFAbWFpbC5jb20iXSwidW52ZXJpZmllZEVtYWlscyI6W119DQoNCiAgICBpZiAocmVjb3JkPy5sb2dpbklkcz8uZW1haWxzICE9PSB1bmRlZmluZWQpIHsNCiAgICAgICAgcmVjb3JkLmxvZ2luSWRzLmVtYWlscyA9IHJlY29yZC5sb2dpbklkcy5lbWFpbHMudHJpbSgpICE9PSAnJyA/DQogICAgICAgICAgICByZWNvcmQubG9naW5JZHMuZW1haWxzLnNwbGl0KCcsJykubWFwKGVtYWlsID0+IGVtYWlsLnRyaW0oKSkgOg0KICAgICAgICAgICAgW107DQogICAgfQ0KDQogICAgLy8gSGFuZGxlICJ1bnZlcmlmaWVkRW1haWxzIiBmaWVsZDogQ29udmVydCB0byBhcnJheSBvciBzZXQgYXMgZW1wdHkgYXJyYXkgaWYgaXQncyBhbiBlbXB0eSBzdHJpbmcNCiAgICBpZiAocmVjb3JkPy5sb2dpbklkcz8udW52ZXJpZmllZEVtYWlscyAhPT0gdW5kZWZpbmVkKSB7DQogICAgICAgIHJlY29yZC5sb2dpbklkcy51bnZlcmlmaWVkRW1haWxzID0gcmVjb3JkLmxvZ2luSWRzLnVudmVyaWZpZWRFbWFpbHMudHJpbSgpICE9PSAnJyA/DQogICAgICAgICAgICByZWNvcmQubG9naW5JZHMudW52ZXJpZmllZEVtYWlscy5zcGxpdCgnLCcpLm1hcChlbWFpbCA9PiBlbWFpbC50cmltKCkpIDoNCiAgICAgICAgICAgIFtdOw0KICAgIH0NCg0KICAgIC8vIEhhbmRsZSAidXNlcm5hbWUiIGZpZWxkOiBTZXQgdG8gbnVsbCBpZiBpdCdzIGFuIGVtcHR5IHN0cmluZw0KICAgIGlmIChyZWNvcmQ/LmxvZ2luSWRzPy51c2VybmFtZSA9PT0gJycpIHsNCiAgICAgICAgcmVjb3JkLmxvZ2luSWRzLnVzZXJuYW1lID0gbnVsbDsNCiAgICB9DQoNCiAgICBsb2dnZXIuaW5mbygibG9naW5JZHMiLCByZWNvcmQubG9naW5JZHMpOw0KICAgIHJldHVybiByZWNvcmQ7DQp9",
@@ -250,7 +250,7 @@ export const azureFullAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "5. Subscriptions"
+    "05. Subscriptions"
    ]
   },
   {
@@ -272,7 +272,7 @@ export const azureFullAccountTemplate = `
    ]
   },
   {
-   "id": "8. Handle unsupported schema",
+   "id": "08. Handle unsupported schema",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7CiAgLy8gRW5zdXJlIGBjb250ZXh0YCBleGlzdHMgYW5kIGlzIHByb3Blcmx5IG5lc3RlZAogIGlmIChyZWNvcmQuaGFzT3duUHJvcGVydHkoJ2NvbnRleHQnKSkgewogICAgcmVjb3JkLmNvbnRleHQgPSB7IGNvbnRleHQ6IHJlY29yZC5jb250ZXh0IH07CiAgfSBlbHNlIGlmICghcmVjb3JkLmhhc093blByb3BlcnR5KCdjb250ZXh0JykpIHsKICAgIHJlY29yZC5jb250ZXh0ID0ge307CiAgfQoKICAvLyBNb3ZlIHNwZWNpZmljIHByb3BlcnRpZXMgdG8gdGhlIGNvbnRleHQgb2JqZWN0CiAgbW92ZVByb3BlcnR5KHJlY29yZCwgcmVjb3JkLmNvbnRleHQsICdjdXN0b21JZGVudGlmaWVycycpOwogIG1vdmVQcm9wZXJ0eShyZWNvcmQsIHJlY29yZC5jb250ZXh0LCAnYWRkcmVzc2VzJyk7CiAgbW92ZVByb3BlcnR5KHJlY29yZCwgcmVjb3JkLmNvbnRleHQsICdpbnRlcm5hbCcpOwoKICByZXR1cm4gcmVjb3JkOwp9CgpmdW5jdGlvbiBtb3ZlUHJvcGVydHkocmVjb3JkLCB0YXJnZXQsIHByb3BlcnR5TmFtZSkgewogIGlmIChyZWNvcmQuaGFzT3duUHJvcGVydHkocHJvcGVydHlOYW1lKSkgewogICAgLy8gTW92ZSB0aGUgcHJvcGVydHkgdG8gdGhlIHRhcmdldCBvYmplY3QKICAgIHRhcmdldFtwcm9wZXJ0eU5hbWVdID0gcmVjb3JkW3Byb3BlcnR5TmFtZV07CiAgICAvLyBEZWxldGUgdGhlIHByb3BlcnR5IGZyb20gdGhlIG9yaWdpbmFsIHJlY29yZAogICAgZGVsZXRlIHJlY29yZFtwcm9wZXJ0eU5hbWVdOwogIH0KfQo=",
@@ -280,7 +280,7 @@ export const azureFullAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "9. Import Account Request Logger"
+    "09. Import Account Request Logger"
    ]
   },
   {
@@ -296,7 +296,7 @@ export const azureFullAccountTemplate = `
    ]
   },
   {
-   "id": "6.3 Lookup Failed",
+   "id": "06.3 Lookup Failed",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQoNCiAgbG9nZ2VyLmluZm8oIkxvb2t1cCBmYWlsZWQgcmVzcG9uc2UiLCByZWNvcmQpOw0KICByZXR1cm4gcmVjb3JkOw0KfQ==",
@@ -309,7 +309,7 @@ export const azureFullAccountTemplate = `
    "error": []
   },
   {
-   "id": "6.1 If UID is Present",
+   "id": "06.1 If UID is Present",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQogICAgLy8gRGV0ZXJtaW5lcyB3aGV0aGVyIGxvb2t1cCBzaG91bGQgYmUgY2FsbGVkIGJhc2VkIG9uIFVJRCB2YWx1ZQ0KDQogICAgaWYgKHJlY29yZC51aWQgJiYgcmVjb3JkLnVpZC50cmltKCkgIT09ICcnKSB7DQogICAgICAgIC8vIFVJRCBoYXMgdmFsdWUuIEV4ZWN1dGUgbG9va3VwDQogICAgICAgIGxvZ2dlci5pbmZvKCJBY2NvdW50IExvb2t1cCBSZXF1ZXN0IFVJRDogIiwgcmVjb3JkLnVpZCk7DQogICAgICAgIHJldHVybiByZWNvcmQ7DQogICAgfQ0KICAgIHJldHVybiBudWxsOyAvLyBVSUQgaXMgQmxhbmsuIFNraXAgTG9va3VwLg0KfQ==",
@@ -317,11 +317,11 @@ export const azureFullAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "6.2 Account lookup using UID"
+    "06.2 Account lookup using UID"
    ]
   },
   {
-   "id": "6. If UID Is Not Present",
+   "id": "06. If UID Is Not Present",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQogICAgLy8gRGV0ZXJtaW5lcyB3aGV0aGVyIGxvb2t1cCBzaG91bGQgYmUgc2tpcHBlZCBiYXNlZCBvbiBVSUQgdmFsdWUNCiAgICBpZiAoIXJlY29yZC51aWQgfHwgcmVjb3JkLnVpZC50cmltKCkgPT09ICcnKSB7DQogICAgICAgIC8vIFVJRCBpcyBlaXRoZXIgbm90IHByZXNlbnQgb3IgYmxhbmsuIFNraXAgbG9va3VwLg0KICAgICAgICByZXR1cm4gcmVjb3JkOw0KICAgIH0NCiAgICByZXR1cm4gbnVsbDsgLy8gVUlEIGhhcyB2YWx1ZS4gRXhlY3V0ZSBsb29rdXAgZWxzZXdoZXJlLg0KfQ==",
@@ -329,7 +329,7 @@ export const azureFullAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "7. Handle UID"
+    "07. Handle UID"
    ]
   }
  ],
--- src/services/importAccounts/serverImport/AzureAccountsTemplates/azureLiteAccountTemplate.js
+++ src/services/importAccounts/serverImport/AzureAccountsTemplates/azureLiteAccountTemplate.js
@@ -6,7 +6,7 @@ export const azureLiteAccountTemplate = `
 
  "steps": [
   {
-   "id": "3. Transform to CDC Structure",
+   "id": "03. Transform to CDC Structure",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQogICAgLy8gY29udmVydHMgaW5wdXQ6ICJwcm9maWxlLmZpcnN0TmFtZSI6IlRlc3QgMSIsInByb2ZpbGUubGFzdE5hbWUiOiJFQiIsInByb2ZpbGUuZWR1Y2F0aW9uLjAuZGVncmVlIjoiR3JhZHVhdGlvbiIsInByb2ZpbGUuZWR1Y2F0aW9uLjAuZmllbGRPZlN0dWR5IjoiU2NpZW5jZSINCiAgICAvLyB0byBvdXRwdXQ6IHsicHJvZmlsZSI6eyJlbWFpbCI6InRlc3Q4ODJAbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJUZXN0IDEiLCJsYXN0TmFtZSI6IkVCIiwiZWR1Y2F0aW9uIjpbeyJkZWdyZWUiOiJHcmFkdWF0aW9uIiwiZmllbGRPZlN0dWR5IjoiU2NpZW5jZSJ9XX0NCiAgICANCiAgICBsb2dnZXIuaW5mbygiSW5wdXQgUGF5bG9hZCIsIHJlY29yZCk7DQoNCiAgICBsZXQgcmVzdWx0ID0ge307DQoNCiAgICBmb3IgKGxldCBrZXkgaW4gcmVjb3JkKSB7DQogICAgICAgIGlmIChyZWNvcmQuaGFzT3duUHJvcGVydHkoa2V5KSkgew0KICAgICAgICAgICAgY29uc3QgdmFsdWUgPSByZWNvcmRba2V5XTsNCiAgICAgICAgICAgIGNvbnN0IGtleXNBcnJheSA9IGtleS5zcGxpdCgnLicpOw0KDQogICAgICAgICAgICBjb25zdCBuZXN0ZWRPYmplY3QgPSBidWlsZE5lc3RlZE9iamVjdChrZXlzQXJyYXksIHZhbHVlKTsNCiAgICAgICAgICAgIHJlc3VsdCA9IG1lcmdlRGVlcChyZXN1bHQsIG5lc3RlZE9iamVjdCk7DQogICAgICAgIH0NCiAgICB9DQoNCiAgICBsb2dnZXIuaW5mbygiVHJhbnNmb3JtZWQgUGF5bG9hZCIsIHJlc3VsdCk7DQogICAgcmV0dXJuIHJlc3VsdDsNCn0NCg0KZnVuY3Rpb24gYnVpbGROZXN0ZWRPYmplY3Qoa2V5cywgdmFsdWUpIHsNCiAgICAvLyBSZWN1cnNpdmUgZnVuY3Rpb24gdG8gYnVpbGQgYSBuZXN0ZWQgb2JqZWN0IG9yIGFycmF5DQogICAgaWYgKGtleXMubGVuZ3RoID09PSAxKSB7DQogICAgICAgIHJldHVybiB7DQogICAgICAgICAgICBba2V5c1swXV06IHZhbHVlDQogICAgICAgIH07DQogICAgfQ0KDQogICAgY29uc3Qga2V5ID0ga2V5c1swXTsNCiAgICBjb25zdCBuZXh0S2V5ID0ga2V5c1sxXTsNCg0KICAgIC8vIElmIG5leHQga2V5IGlzIGEgbnVtYmVyLCB0cmVhdCBpdCBhcyBhbiBhcnJheQ0KICAgIGlmICghaXNOYU4ocGFyc2VJbnQobmV4dEtleSkpKSB7DQogICAgICAgIGxldCBhcnIgPSBbXTsNCiAgICAgICAgYXJyW3BhcnNlSW50KG5leHRLZXkpXSA9IGJ1aWxkTmVzdGVkT2JqZWN0KGtleXMuc2xpY2UoMiksIHZhbHVlKTsNCiAgICAgICAgcmV0dXJuIHsNCiAgICAgICAgICAgIFtrZXldOiBhcnINCiAgICAgICAgfTsNCiAgICB9DQoNCiAgICByZXR1cm4gew0KICAgICAgICBba2V5XTogYnVpbGROZXN0ZWRPYmplY3Qoa2V5cy5zbGljZSgxKSwgdmFsdWUpDQogICAgfTsNCn0NCg0KZnVuY3Rpb24gbWVyZ2VEZWVwKHRhcmdldCwgc291cmNlKSB7DQogICAgLy8gSGVscGVyIGZ1bmN0aW9uIHRvIG1lcmdlIG5lc3RlZCBvYmplY3RzIG9yIGFycmF5cw0KICAgIGZvciAobGV0IGtleSBpbiBzb3VyY2UpIHsNCiAgICAgICAgaWYgKHNvdXJjZVtrZXldIGluc3RhbmNlb2YgT2JqZWN0ICYmIGtleSBpbiB0YXJnZXQpIHsNCiAgICAgICAgICAgIGlmIChBcnJheS5pc0FycmF5KHRhcmdldFtrZXldKSAmJiBBcnJheS5pc0FycmF5KHNvdXJjZVtrZXldKSkgew0KICAgICAgICAgICAgICAgIC8vIE1lcmdlIGFycmF5cyBieSBpbmRleCwgbWFraW5nIHN1cmUgdG8gYXZvaWQgbnVsbCB2YWx1ZXMNCiAgICAgICAgICAgICAgICB0YXJnZXRba2V5XSA9IHRhcmdldFtrZXldLm1hcCgoaXRlbSwgaW5kZXgpID0+IHsNCiAgICAgICAgICAgICAgICAgICAgcmV0dXJuIGl0ZW0gIT09IHVuZGVmaW5lZCA/IG1lcmdlRGVlcChpdGVtLCBzb3VyY2Vba2V5XVtpbmRleF0pIDogc291cmNlW2tleV1baW5kZXhdOw0KICAgICAgICAgICAgICAgIH0pLmNvbmNhdChzb3VyY2Vba2V5XS5zbGljZSh0YXJnZXRba2V5XS5sZW5ndGgpKTsNCiAgICAgICAgICAgIH0gZWxzZSB7DQogICAgICAgICAgICAgICAgT2JqZWN0LmFzc2lnbihzb3VyY2Vba2V5XSwgbWVyZ2VEZWVwKHRhcmdldFtrZXldLCBzb3VyY2Vba2V5XSkpOw0KICAgICAgICAgICAgfQ0KICAgICAgICB9IGVsc2Ugew0KICAgICAgICAgICAgdGFyZ2V0W2tleV0gPSBzb3VyY2Vba2V5XTsNCiAgICAgICAgfQ0KICAgIH0NCiAgICByZXR1cm4gdGFyZ2V0Ow0KfQ==",
@@ -14,11 +14,11 @@ export const azureLiteAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "4. Transform Subscriptions"
+    "04. Transform Subscriptions"
    ]
   },
   {
-   "id": "1. Read azure.blob",
+   "id": "01. Read azure.blob",
    "type": "datasource.read.azure.blob",
    "params": {
     "accountName": "{{accountName}}",
@@ -28,11 +28,11 @@ export const azureLiteAccountTemplate = `
     "blobPrefix": "{{blobPrefix}}"
    },
    "next": [
-    "2. file.parse.dsv"
+    "02. file.parse.dsv"
    ]
   },
   {
-   "id": "2. file.parse.dsv",
+   "id": "02. file.parse.dsv",
    "type": "file.parse.dsv",
    "params": {
     "columnSeparator": ",",
@@ -41,11 +41,11 @@ export const azureLiteAccountTemplate = `
     "addFilename": false
    },
    "next": [
-    "3. Transform to CDC Structure"
+    "03. Transform to CDC Structure"
    ]
   },
   {
-   "id": "4. Transform Subscriptions",
+   "id": "04. Transform Subscriptions",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQogICAgLy8gQ29udmVydHMgaW5wdXQ6ICJzdWJzY3JpcHRpb25zLm5ld3NsZXR0ZXIuY29tbWVyY2lhbC5pc1N1YnNjcmliZWQiOiJUUlVFIg0KICAgIC8vIHRvIG91dHB1dDogeyJzdWJzY3JpcHRpb25zIjogeyJuZXdzbGV0dGVyIjp7ImNvbW1lcmNpYWwiOnsiZW1haWwiOnsiaXNTdWJzY3JpYmVkIjoiVFJVRSJ9fX19fQ0KICAgIC8vIHZhciB4ID0gY3R4LmdldFNoYXJlZFZhcmlhYmxlKCJpbnQiKTsNCiAgICBpZiAocmVjb3JkLmhhc093blByb3BlcnR5KCJzdWJzY3JpcHRpb25zIikpIHsNCiAgICAgICAgcmVjb3JkLnN1YnNjcmlwdGlvbnMgPSBhZGRFbWFpbEJlZm9yZUlzU3Vic2NyaWJlZChyZWNvcmQuc3Vic2NyaXB0aW9ucyk7DQogICAgICAgIGxvZ2dlci5pbmZvKCJUcmFuc2Zvcm1lZCBTdWJzY3JpcHRpb25zIiwgcmVjb3JkLnN1YnNjcmlwdGlvbnMpOw0KICAgIH0NCg0KICAgIHJldHVybiByZWNvcmQ7DQp9DQoNCmZ1bmN0aW9uIGFkZEVtYWlsQmVmb3JlSXNTdWJzY3JpYmVkKG9iaikgew0KICAgIC8vIFJlY3Vyc2l2ZSBmdW5jdGlvbiB0byB0cmF2ZXJzZSBhbmQgZmluZCB0aGUgb2JqZWN0IHdpdGggImlzU3Vic2NyaWJlZCINCiAgICBmdW5jdGlvbiB0cmF2ZXJzZShub2RlKSB7DQogICAgICAgIGlmICh0eXBlb2Ygbm9kZSAhPT0gJ29iamVjdCcgfHwgbm9kZSA9PT0gbnVsbCkgcmV0dXJuOw0KDQogICAgICAgIGNvbnN0IGtleXMgPSBPYmplY3Qua2V5cyhub2RlKTsNCg0KICAgICAgICAvLyBDaGVjayBpZiB0aGUgY3VycmVudCBub2RlIGhhcyAiaXNTdWJzY3JpYmVkIiBrZXkNCiAgICAgICAgaWYgKGtleXMuaW5jbHVkZXMoImlzU3Vic2NyaWJlZCIpKSB7DQogICAgICAgICAgICBjb25zdCBjb3B5ID0gew0KICAgICAgICAgICAgICAgIC4uLm5vZGUNCiAgICAgICAgICAgIH07DQoNCiAgICAgICAgICAgIC8vIENsZWFyIHRoZSBjdXJyZW50IG9iamVjdCBhbmQgd3JhcCBpdCB3aXRoICJlbWFpbCINCiAgICAgICAgICAgIGZvciAoY29uc3Qga2V5IG9mIGtleXMpIHsNCiAgICAgICAgICAgICAgICBkZWxldGUgbm9kZVtrZXldOw0KICAgICAgICAgICAgfQ0KDQogICAgICAgICAgICBub2RlLmVtYWlsID0gY29weTsNCiAgICAgICAgICAgIHJldHVybjsNCiAgICAgICAgfQ0KDQogICAgICAgIC8vIFJlY3Vyc2l2ZWx5IHRyYXZlcnNlIHRoZSBjaGlsZCBub2Rlcw0KICAgICAgICBmb3IgKGNvbnN0IGtleSBvZiBrZXlzKSB7DQogICAgICAgICAgICBpZiAodHlwZW9mIG5vZGVba2V5XSA9PT0gJ29iamVjdCcgJiYgbm9kZVtrZXldICE9PSBudWxsKSB7DQogICAgICAgICAgICAgICAgdHJhdmVyc2Uobm9kZVtrZXldKTsNCiAgICAgICAgICAgIH0NCiAgICAgICAgfQ0KICAgIH0NCg0KICAgIC8vIFN0YXJ0IHRyYXZlcnNhbCBmcm9tIHRoZSByb290IG9iamVjdA0KICAgIHRyYXZlcnNlKG9iaik7DQoNCiAgICByZXR1cm4gb2JqOw0KfQ==",
@@ -53,11 +53,11 @@ export const azureLiteAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "5. Build Payload"
+    "05. Build Payload"
    ]
   },
   {
-   "id": "7.2 Error File",
+   "id": "07.2 Error File",
    "type": "file.format.dsv",
    "params": {
     "fileName": "Error_importLiteAccount_\${now}.csv",
@@ -84,7 +84,7 @@ export const azureLiteAccountTemplate = `
    }
   },
   {
-   "id": "7. gigya.generic - ImportLiteAccount",
+   "id": "07. gigya.generic - ImportLiteAccount",
    "type": "datasource.write.gigya.generic",
    "params": {
     "apiMethod": "accounts.importLiteAccount",
@@ -124,14 +124,14 @@ export const azureLiteAccountTemplate = `
     ]
    },
    "next": [
-    "8. Import Account Success Response"
+    "08. Import Account Success Response"
    ],
    "error": [
-    "7.1 Import Account Error Response"
+    "07.1 Import Account Error Response"
    ]
   },
   {
-   "id": "8. Import Account Success Response",
+   "id": "08. Import Account Success Response",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQoNCiAgbG9nZ2VyLmluZm8oIkltcG9ydCBBY2NvdW50IFJlc3BvbnNlIiwgcmVjb3JkKTsNCiAgcmV0dXJuIHJlY29yZDsNCn0=",
@@ -139,11 +139,11 @@ export const azureLiteAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "9. Success File"
+    "09. Success File"
    ]
   },
   {
-   "id": "7.1 Import Account Error Response",
+   "id": "07.1 Import Account Error Response",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQoNCiAgbG9nZ2VyLmluZm8oIkltcG9ydCBBY2NvdW50IEVycm9yIFJlc3BvbnNlIiwgcmVjb3JkKTsNCiAgcmV0dXJuIHJlY29yZDsNCn0=",
@@ -151,11 +151,11 @@ export const azureLiteAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "7.2 Error File"
+    "07.2 Error File"
    ]
   },
   {
-   "id": "6. Import Account Request Logger",
+   "id": "06. Import Account Request Logger",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQoNCiAgbG9nZ2VyLmluZm8oIkltcG9ydCBBY2NvdW50IFJlcXVlc3QiLCByZWNvcmQpOw0KICAgIHJldHVybiByZWNvcmQ7DQp9",
@@ -163,11 +163,11 @@ export const azureLiteAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "7. gigya.generic - ImportLiteAccount"
+    "07. gigya.generic - ImportLiteAccount"
    ]
   },
   {
-   "id": "9. Success File",
+   "id": "09. Success File",
    "type": "file.format.dsv",
    "params": {
     "fileName": "Success_importLiteAccount_\${now}.csv",
@@ -185,7 +185,7 @@ export const azureLiteAccountTemplate = `
    ]
   },
   {
-   "id": "5. Build Payload",
+   "id": "05. Build Payload",
    "type": "record.evaluate",
    "params": {
     "script": "ZnVuY3Rpb24gcHJvY2VzcyhyZWNvcmQsIGN0eCwgbG9nZ2VyLCBuZXh0KSB7DQovLyByZWNvcmQuZW1haWwgPSByZWNvcmQucHJvZmlsZS5lbWFpbDsNCiAgICBjb25zdCByZXF1ZXN0ID0gew0KICAgICAgICBwcm9maWxlOiB7fSwNCiAgICAgICAgZGF0YToge30sDQogICAgICAgIHN1YnNjcmlwdGlvbnM6IHt9LA0KICAgICAgICBwcmVmZXJlbmNlczoge30sDQogICAgICAgIGNvbnRleHQ6IG51bGwNCiAgICB9Ow0KDQogICAgLy8gQXNzaWduIHZhbHVlcyBmcm9tIGByZWNvcmRgIHRvIGByZXF1ZXN0YCBpZiBwcmVzZW50IGFuZCB2YWxpZA0KICAgIGFzc2lnbklmUHJlc2VudChyZXF1ZXN0LCAncHJvZmlsZScsIHJlY29yZCk7DQogICAgYXNzaWduSWZQcmVzZW50KHJlcXVlc3QsICdkYXRhJywgcmVjb3JkKTsNCiAgICBhc3NpZ25JZlByZXNlbnQocmVxdWVzdCwgJ3N1YnNjcmlwdGlvbnMnLCByZWNvcmQpOw0KICAgIGFzc2lnbklmUHJlc2VudChyZXF1ZXN0LCAncHJlZmVyZW5jZXMnLCByZWNvcmQpOw0KICAgIGFzc2lnbklmUHJlc2VudChyZXF1ZXN0LCAnY29udGV4dCcsIHJlY29yZCk7DQoNCiAgICByZWNvcmQucmVxdWVzdCA9IHJlcXVlc3Q7DQoNCiAgICBsb2dnZXIuaW5mbygiVHJhbnNmb3JtZWQgUmVxdWVzdCBQYXlsb2FkIiwgcmVjb3JkKTsNCiAgICByZXR1cm4gcmVjb3JkOw0KfQ0KDQovLyBIZWxwZXIgZnVuY3Rpb24gdG8gYXNzaWduIHZhbHVlIGlmIGl0IGV4aXN0cywgaXMgbm90IG51bGwsIGFuZCBpcyBub3QgYW4gZW1wdHkgc3RyaW5nDQpmdW5jdGlvbiBhc3NpZ25JZlByZXNlbnQodGFyZ2V0LCBrZXksIHNvdXJjZSkgew0KICAgIGlmIChzb3VyY2UgJiYgc291cmNlW2tleV0gIT09IHVuZGVmaW5lZCAmJiBzb3VyY2Vba2V5XSAhPT0gbnVsbCAmJiBzb3VyY2Vba2V5XSAhPT0gJycpIHsNCiAgICAgICAgdGFyZ2V0W2tleV0gPSBzb3VyY2Vba2V5XTsNCiAgICB9DQp9",
@@ -193,7 +193,7 @@ export const azureLiteAccountTemplate = `
     "notifyLastRecord": false
    },
    "next": [
-    "6. Import Account Request Logger"
+    "06. Import Account Request Logger"
    ]
   }
  ],
