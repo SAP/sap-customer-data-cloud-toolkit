1ce538b0e749129b2a534a1ead1484cfdec3d366
Tiago Gaspar <tiago.gaspar@sap.com>
2025-03-25 12:04:30 +0000

    feature/CDCTOOLBOX-441: add success message verification for backup completion

--- cypress/e2e/versionControl.cy.js
+++ cypress/e2e/versionControl.cy.js
@@ -55,7 +55,32 @@ describe('Version Control Test Suite', () => {
     cy.get('#backupDialog').find('#header').should('contain.text', 'Backup Information')
     cy.get('#backupDialog').find('li').should('have.length', '16')
     cy.get('#backupDialog').find('ui5-textarea').shadow().find('textArea').should('be.visible').type('test commit message')
+    cy.get('[data-cy="confirmBackupButton"]').click()
+    cy.get('#versionControlSuccessPopup').should('be.visible').should('contain.text', 'Backup completed successfully!')
   })
+
+  // it.only('should display an error Popup when reverting', () => {
+  //   utils.mockGetConfigurationRequests()
+  //   cy.intercept('GET', `${url}/repos/testOwner/testRepo/branches`, { body: dataTest.mockedVersionControlGetListBranches }).as('getBranches')
+  //   cy.intercept('GET', `${url}/repos/testOwner/testRepo/branches/main`, { body: dataTest.mockedVersionControlGetResponse })
+  //   cy.intercept('GET', `${url}https://api.github.com/repos/testOwner/testRepo/commits/testSha`, { body: dataTest.mockedVersionControlGetCommitsResponse[0] })
+  //   cy.intercept('GET', `${url}https://api.github.com/repos/testOwner/testRepo/commits/testSha`, { body: dataTest.mockedVersionControlGetResponse })
+  //   cy.intercept('GET', `${url}/repos/testOwner/testRepo/commits?sha=undefined&per_page=100&page=1`, {
+  //     body: dataTest.mockedVersionControlGetCommitsResponse,
+  //   }).as('getCommits')
+  //   cy.intercept('GET', `${url}/user`, {
+  //     body: { callId: 'ea4861dc2cab4c01ab265ffe3eab6c71', errorCode: 0, apiVersion: 2, statusCode: 200, statusReason: 'OK', login: 'testOwner' },
+  //   })
+
+  //   cy.intercept('OPTIONS', `${url}/repos/testOwner/testRepo/git/refs/heads%2FtestApiKey`, {})
+  //   cy.get('#ownerInput').should('be.visible').shadow().find('input').type('testOwner')
+  //   cy.get('#gitTokenInput').should('be.visible').shadow().find('input').type('testToken')
+  //   cy.get('[data-cy="repoInput"]').should('be.visible').shadow().find('input').type('testRepo{enter}')
+  //   cy.get('[data-cy="backupButton"]').should('not.be.disabled')
+  //   cy.get('#versionControlTable').should('be.visible').should('contain.text', 'Create test CDCRepo branch creation')
+  //   cy.get('#commitRevertButton-0').should('be.visible').click()
+  //   cy.get('#versionControlSuccessPopup').should('be.visible').should('contain.text', 'Restore completed successfully!')
+  // })
 
   it('should display credentials input fields', () => {
     cy.get('#ownerInput').should('be.visible')
